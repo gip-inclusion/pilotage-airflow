1e85ae113f91becb2b503aac239b35ba3aed0619
laurinehu <laurinehu@gmail.com>
2023-02-20 13:06:10 +0100

    add informations about structures support (accompagnement)

--- scripts/sql/igas_structure_eiti.sql
+++ scripts/sql/igas_structure_eiti.sql
@@ -50,6 +50,35 @@ etp_cons_par_struct as (
         id_struct,
         annee_af
 ),
+-- accompagnement spécifique par structure
+acc_par_structure as (
+select 
+    structure.structure_id_siae id_struct,
+    sum(acc_dif_nb_sal_conc_illetrisme) nb_salaries_concernes_illetrisme,
+    sum(acc_dif_nb_sal_int_ext_illetrisme) nb_salaries_accompagnes_illetrisme,
+    sum(acc_dif_nb_sal_conc_sante) nb_salaries_concernes_sante,
+    sum(acc_dif_nb_sal_int_ext_sante) nb_salaries_accompagnes_sante,
+    sum(acc_dif_nb_sal_conc_heberg) nb_salaries_concernes_hebergement,
+    sum(acc_dif_nb_sal_int_ext_heberg) nb_salaries_accompagnes_hebergement,
+    sum(acc_dif_nb_sal_conc_demarch) nb_salaries_concernes_demarches_admin,
+    sum("acc_dif_nb_sal_int_ext_demarch ") nb_salaries_accompagnes_demarches_admin,
+    sum(acc_dif_nb_sal_conc_mobilite) nb_salaries_concernes_mobilite,
+    sum(acc_dif_nb_sal_int_ext_mobilite) nb_salaries_accompagnes_mobilite,
+    sum(acc_dif_nb_sal_conc_surendet) nb_salaries_concernes_surendetement,
+    sum(acc_dif_nb_sal_int_ext_surendet) nb_salaries_accompagnes_surendetement,
+    -- bug avec les deux colonnes ci dessous oO
+    --sum("acc_dif_nb_sal_conc_justice ") nb_salaries_concernes_justice,
+    --sum("acc.acc_dif_nb_sal_int_ext_justice ") nb_salaries_accompagnes_justice,
+    sum(acc_dif_nb_sal_conc_manque_dispo) nb_salaries_concernes_manque_dispo,
+    sum(acc_dif_nb_sal_int_ext_manque_dispo) nb_salaries_accompagnes_manque_dispo
+    from
+    "FluxIAE_accompagnement" acc
+    left join "fluxIAE_AnnexeFinanciere_v2" as af on af.af_id_annexe_financiere = acc.acc_afi_id
+    left join "fluxIAE_Structure_v2" as structure on af.af_id_structure = structure.structure_id_siae
+    where af.type_siae = 'EITI'
+    group by structure.structure_id_siae
+    order by structure.structure_id_siae
+),
 -- contrats par structure
 contrat_par_structure as (
     select
@@ -74,14 +103,15 @@ group by
 )
 select
     cps.*,
-    cons.annee_af,
     conv.effectif_annuel_conv,
     cons.nombre_etp_consommes_reels_annuels,
     cons.nombre_etp_consommes_asp,
     conv.effectif_mensuel_conv,
-    cons.nombre_etp_consommes_reels_mensuels
+    cons.nombre_etp_consommes_reels_mensuels,
+    aps.* -- accompagnement par structure
 from
     contrat_par_structure cps
+    left join acc_par_structure aps on cps.id_struct = aps.id_struct
     left join etp_cons_par_struct cons on cps.id_struct = cons.id_struct and cps.annee_embauche = cons.annee_af
     left join etp_conv_par_struct conv on cons.id_struct = conv.id_struct
         and cons.annee_af = conv.annee_af;
