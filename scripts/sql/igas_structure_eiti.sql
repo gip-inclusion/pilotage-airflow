557574b8ee496c8eea508533ecb3637d14e09ba6
laurinehu <laurinehu@gmail.com>
2023-02-20 12:35:29 +0100

    correct etp calculation for structure table

--- scripts/sql/igas_structure_eiti.sql
+++ scripts/sql/igas_structure_eiti.sql
@@ -1,23 +1,62 @@
-with etp_par_structure as (
-    select
-        af.af_id_structure as id_struct,
-        date_part('year', etp_r.date_validation_declaration) as annee_declaration,        
-        sum(etp_r.nombre_etp_consommes_asp) as etp_consommes_asp,
-        sum(etp_r.nombre_etp_consommes_reels_annuels) as nombre_etp_consommes_reels_annuels,
-        sum(etp_r.nombre_etp_consommes_reels_mensuels) as nombre_etp_consommes_reels_mensuels
+-- etp conventionnes 
+with etp_conv_par_struct as (
+    select distinct
+        structure.structure_id_siae as id_struct,
+        date_part('year',
+            af.af_date_debut_effet_v2) as annee_af,
+        sum(af.af_etp_postes_insertion) as effectif_mensuel_conv,
+        sum((af.af_etp_postes_insertion * ((date_part('year',
+                    af_date_fin_effet_v2) - date_part('year',
+                    af_date_debut_effet_v2)) * 12 + (date_part('month',
+                    af_date_fin_effet_v2) - date_part('month',
+                    af_date_debut_effet_v2)) + 1) / 12)) as effectif_annuel_conv
     from
-        "suivi_etp_realises_v2" etp_r
-    left join "fluxIAE_AnnexeFinanciere_v2" af on af.af_id_annexe_financiere = etp_r.id_annexe_financiere
-where
-    type_structure = 'EITI DC'
-group by
-    af.af_id_structure,
-    annee_declaration
+        "fluxIAE_AnnexeFinanciere_v2" as af
+    left join "fluxIAE_Structure_v2" as structure on af.af_id_structure = structure.structure_id_siae
+    where
+        af.af_mesure_dispositif_code = 'EITI_DC'
+        --date_part('year', af.af_date_debut_effet_v2) >= annee_en_cours - 2
+        and af.af_etat_annexe_financiere_code in('VALIDE',
+            'PROVISOIRE',
+            'CLOTURE')
+        and af_mesure_dispositif_code not like '%FDI%'
+    group by
+        id_struct,
+        annee_af
 ),
+-- etp realises
+etp_cons_par_struct as (
+    select
+        structure.structure_id_siae as id_struct,
+        date_part('year',
+            af.af_date_debut_effet_v2) as annee_af,
+        sum(emi.emi_part_etp) as nombre_etp_consommes_asp,
+        sum(emi.emi_nb_heures_travail) as nombre_heures_travaillees,
+        sum(emi.emi_nb_heures_travail / firmi.rmi_valeur) as nombre_etp_consommes_reels_mensuels,
+        sum(emi.emi_nb_heures_travail / firmi.rmi_valeur * 12) as nombre_etp_consommes_reels_annuels
+    from
+        "fluxIAE_EtatMensuelIndiv" as emi
+    left join "fluxIAE_AnnexeFinanciere_v2" as af on emi.emi_afi_id = af.af_id_annexe_financiere
+    left join "fluxIAE_RefMontantIae" firmi on af_mesure_dispositif_id = firmi.rme_id
+    left join "fluxIAE_Structure_v2" as structure on af.af_id_structure = structure.structure_id_siae
+    where
+        af.af_mesure_dispositif_code = 'EITI_DC'
+        and firmi.rmi_libelle = 'Nombre d''heures annuelles théoriques pour un salarié à taux plein'
+        and af.af_etat_annexe_financiere_code in('VALIDE',
+            'PROVISOIRE',
+            'CLOTURE')
+        and af_mesure_dispositif_code not like '%FDI%'
+    group by
+        id_struct,
+        annee_af
+),
+-- contrats par structure
 contrat_par_structure as (
     select
         contrat_mission.contrat_id_structure as id_struct,
-        date_part('year', to_date(contrat_mission.contrat_date_embauche, 'DD/MM/YYYY')) as annee_embauche,
+        date_part('year',
+            to_date(contrat_mission.contrat_date_embauche,
+                'DD/MM/YYYY')) as annee_embauche,
         count(*) as nb_contrats,
         count(formations.*) as nb_formations,
         sum(formations.formation_duree_jours) as nb_jours_formation,
@@ -34,7 +73,15 @@ group by
     annee_embauche
 )
 select
-    *
+    cps.*,
+    cons.annee_af,
+    conv.effectif_annuel_conv,
+    cons.nombre_etp_consommes_reels_annuels,
+    cons.nombre_etp_consommes_asp,
+    conv.effectif_mensuel_conv,
+    cons.nombre_etp_consommes_reels_mensuels
 from
-    etp_par_structure etp_s
-    left join contrat_par_structure contrat_s on etp_s.id_struct = contrat_s.id_struct and etp_s.annee_declaration = contrat_s.annee_embauche;
+    contrat_par_structure cps
+    left join etp_cons_par_struct cons on cps.id_struct = cons.id_struct and cps.annee_embauche = cons.annee_af
+    left join etp_conv_par_struct conv on cons.id_struct = conv.id_struct
+        and cons.annee_af = conv.annee_af;
