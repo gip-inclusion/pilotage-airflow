24fcc3a3600529e7299dd0bacf7c1b3a64827e21
laurinehu <laurinehu@gmail.com>
2023-02-20 15:19:22 +0100

    add structure denomination and af id

--- scripts/sql/igas_structure_eiti.sql
+++ scripts/sql/igas_structure_eiti.sql
@@ -2,6 +2,8 @@
 with etp_conv_par_struct as (
     select distinct
         structure.structure_id_siae as id_struct,
+        structure.structure_denomination as denomination_structure,
+        af.af_numero_annexe_financiere as id_annexe_financiere,
         date_part('year',
             af.af_date_debut_effet_v2) as annee_af,
         sum(af.af_etp_postes_insertion) as effectif_mensuel_conv,
@@ -22,6 +24,8 @@ with etp_conv_par_struct as (
         and af_mesure_dispositif_code not like '%FDI%'
     group by
         id_struct,
+        structure.structure_denomination,
+        af.af_numero_annexe_financiere,
         annee_af
 ),
 -- etp realises
@@ -82,7 +86,7 @@ select
 -- contrats par structure
 contrat_par_structure as (
     select
-        contrat_mission.contrat_id_structure as id_struct,
+        contrat_mission.contrat_id_structure as id_struct,       
         date_part('year',
             to_date(contrat_mission.contrat_date_embauche,
                 'DD/MM/YYYY')) as annee_embauche,
@@ -131,6 +135,8 @@ sorties_par_structure as (
         id_structure_asp
 )
 select
+    conv.denomination_structure,
+    conv.id_annexe_financiere,
     cps.*,
     conv.effectif_annuel_conv,
     cons.nombre_etp_consommes_reels_annuels,
