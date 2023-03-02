805db64ba117f79197b03ecd793749554120cfb0
laurinehu <laurinehu@gmail.com>
2023-03-02 18:05:12 +0100

    igas: solve counting contracts pb

--- scripts/sql/igas_contrat_par_eiti.sql
+++ scripts/sql/igas_contrat_par_eiti.sql
@@ -3,9 +3,9 @@ create table igas_contrat_par_eiti as
 select
     contrat_mission.contrat_id_structure as id_struct,
     date_part('year', to_date(contrat_mission.contrat_date_embauche, 'DD/MM/YYYY')) as annee_embauche,
-    count(*) as nb_contrats,
+    count(distinct contrat_mission.contrat_id_ctr) as nb_contrats,
     count(distinct contrat_mission.contrat_id_pph) as nb_salaries,
-    count(formations.*) as nb_formations,
+    count(formations.formation_id_for) as nb_formations,
     sum(formations.formation_duree_jours) as nb_jours_formation,
     sum(formations.formation_duree_heures) as nb_heures_formation,
     sum(formations.formation_duree_minutes) as nb_min_formation
