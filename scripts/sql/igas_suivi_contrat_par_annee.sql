9999b673f4bff25f64366d7bd88b3df14d3be85b
laurinehu <laurinehu@gmail.com>
2023-03-06 11:36:15 +0100

    igas: aggregate salarie info by contract

---
+++ scripts/sql/igas_suivi_contrat_par_annee.sql
@@ -0,0 +1,32 @@
+create table igas_suivi_contrat_par_annee as 
+select
+    contrat_id,
+    annee_af,
+    sum(nombre_heures_travaillees) as nombre_heures_travaillees,
+    sum(nombre_etp_consommes_reels_annuels) as nombre_etp_consommes_reels_annuels,
+    civilite,
+    annee_de_naissance,
+    sum(nb_formations) nb_formations,
+    sum(nb_jours_formation) nb_jours_formation,
+    sum(nb_heures_formation) nb_heures_formation,
+    rqth,
+    aah,
+    oeth,
+    ass,
+    zrr,
+    aide_soc,
+    qpv
+from
+    igas_eiti_salarie
+group by
+    contrat_id,
+    annee_af,
+    civilite,
+    annee_de_naissance,
+    rqth,
+    aah,
+    oeth,
+    ass,
+    zrr,
+    aide_soc,
+    qpv;
