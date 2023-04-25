310f722790a9151809d28b9284e28497977bb216
laurinehu <laurinehu@gmail.com>
2023-04-25 19:40:05 +0200

    update seeds names

--- dbt/models/staging/stg_insee_appartenance_geo_communes.sql
+++ dbt/models/staging/stg_insee_appartenance_geo_communes.sql
@@ -9,16 +9,16 @@ select
     epci_libelle."libelle"    as type_epci,
     arrondissements."LIBELLE" as nom_arrondissement,
     zone_emploi."LIBZE2020"   as nom_zone_emploi
-from {{ ref('communes') }} as communes
-left join {{ ref('regions') }} as regions
+from {{ ref('insee_communes') }} as communes
+left join {{ ref('insee_regions') }} as regions
     on regions."REG" = communes.code_region
-left join {{ ref('departements') }} as departements
+left join {{ ref('insee_departements') }} as departements
     on departements."DEP" = communes.code_dept
-left join {{ ref('epci') }} as epci
+left join {{ ref('insee_epcis') }} as epci
     on epci."EPCI" = communes.code_epci
-left join {{ ref('epci_libelle') }} as epci_libelle
+left join {{ ref('insee_epci_libelles') }} as epci_libelle
     on epci."NATURE_EPCI" = epci_libelle.code_type_epci
-left join {{ ref('arrondissements') }} as arrondissements
+left join {{ ref('insee_arrondissements') }} as arrondissements
     on trim('0' from arrondissements."ARR") = communes.code_arrondissement
-left join {{ ref('zone_emploi') }} as zone_emploi
+left join {{ ref('insee_zones_emploi') }} as zone_emploi
     on zone_emploi."ZE2020" = communes.code_zone_emploi
