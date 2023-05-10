61ce80b673f348d0f343f284224305f5ac8d49ed
laurinehu <laurinehu@gmail.com>
2023-05-10 14:53:45 +0200

    sqlfluff 2nd try

--- dbt/models/marts/organisations_dihal.sql
+++ dbt/models/marts/organisations_dihal.sql
@@ -1,4 +1,3 @@
-select
-    {{ dbt_utils.star(source('emplois', 'organisations')) }}
+select {{ dbt_utils.star(source('emplois', 'organisations')) }}
 from {{ source('emplois', 'organisations') }}
 where type in ('CHRS', 'CHU', 'RS_FJT', 'OIL')
