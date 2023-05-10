eee6db395192e93ae822c810de78bae5120dbdce
YannickPassa <yannick.passarelli@gmail.com>
2023-05-10 17:19:14 +0200

    new file, formatted to fit fbt standards

---
+++ dbt/models/marts/suivi_auto_prescription.sql
@@ -0,0 +1,11 @@
+select
+    {{ dbt_utils.star(ref('stg_autoprescription')) }},
+    s.siret                 as siret,
+    s.active                as active,
+    s.ville                 as ville,
+    s.nom_structure_complet as "nom_structure_complet"
+from
+    {{ ref('stg_autoprescription') }} as autopr_all
+left join
+    {{ ref('stg_structures') }} as s
+    on autopr_all.id_structure = s.id
