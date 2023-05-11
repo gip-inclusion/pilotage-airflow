073d31c7824caec2d234d594c1e6aecb7aa05580
YannickPassa <yannick.passarelli@gmail.com>
2023-05-11 14:34:51 +0200

    auto prescription following : final table

--- dbt/models/marts/suivi_auto_prescription.sql
+++ dbt/models/marts/suivi_auto_prescription.sql
@@ -1,90 +1,15 @@
-with autopr_all as (
-    select
-        c.id,
-        cd.id                                  as id_candidature,
-        c.hash_nir,
-        c.age,
-        c."département",
-        c."nom_département",
-        c."région",
-        c.adresse_en_qpv,
-        c.total_candidatures,
-        c.total_embauches,
-        c.date_diagnostic,
-        cd.date_candidature,
-        c.id_auteur_diagnostic_employeur,
-        c.type_auteur_diagnostic,
-        c.sous_type_auteur_diagnostic,
-        c.nom_auteur_diagnostic,
-        cd."état",
-        cd.id_structure,
-        cd.origine,
-        cd."origine_détaillée",
-        cd.type_structure,
-        cd.nom_structure,
-        cd."département_structure",
-        cd."nom_département_structure",
-        cd."région_structure",
-        date_part('year', c.date_diagnostic)   as "année_diagnostic",
-        date_part('year', cd.date_candidature) as "année_candidature",
-        /* on considère que l'on a de l'auto prescription lorsque l'employeur est l'auteur du diagnostic et effectue l'embauche */
-        /* En créant une colonne on peut comparer les candidatures classiques à l'auto prescription */
-        case
-            when
-                c.type_auteur_diagnostic = 'Employeur'
-                and cd.origine = 'Employeur'
-                and c.id_auteur_diagnostic_employeur = cd.id_structure then 'Autoprescription'
-            else 'parcours classique'
-        end                                    as type_de_candidature,
-        case
-            when c.injection_ai = 0 then 'Non'
-            else 'Oui'
-        end                                    as reprise_de_stock_ai_candidats,
-        case
-            when cd.injection_ai = 0 then 'Non'
-            else 'Oui'
-        end                                    as reprise_de_stock_ai_candidatures
-    from
-        {{ source('emplois', 'candidatures') }} as cd
-    left join
-        {{ source('emplois', 'candidats') }} as c
-        on
-            cd.id_candidat = c.id
-)
-
 select
-    autopr_all.id,
-    id_candidature,
-    type_de_candidature,
-    hash_nir,
-    autopr_all."département",
-    autopr_all."nom_département",
-    autopr_all."région",
-    adresse_en_qpv,
-    date_diagnostic,
-    "année_diagnostic",
-    date_candidature,
-    "année_candidature",
-    id_auteur_diagnostic_employeur,
-    type_auteur_diagnostic,
-    sous_type_auteur_diagnostic,
-    nom_auteur_diagnostic,
-    "état",
-    origine,
-    "origine_détaillée",
-    autopr_all.id_structure,
-    s.siret,
-    s.active,
-    type_structure,
-    nom_structure,
-    s.ville,
-    "département_structure",
-    "nom_département_structure",
-    "région_structure",
-    reprise_de_stock_ai_candidats,
-    reprise_de_stock_ai_candidatures
+    {% if env_var('CI', ',') %}
+        *,
+    {% else %}
+        {{ dbt_utils.star(ref('stg_autoprescription')) }},
+    {% endif %}
+    s.siret                 as siret,
+    s.active                as active,
+    s.ville                 as ville,
+    s.nom_structure_complet as "nom_structure_complet"
 from
-    autopr_all
+    {{ ref('stg_autoprescription') }} as autopr_all
 left join
-    {{ source('emplois', 'structures') }} as s
+    {{ ref('stg_structures') }} as s
     on autopr_all.id_structure = s.id
