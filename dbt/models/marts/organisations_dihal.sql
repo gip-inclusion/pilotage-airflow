9a8469865957e3c4945477a0458b7728b56337de
dejafait <dejafait@blackhole.com>
2023-05-11 12:27:54 +0200

    break CI on purpose to test theory

--- dbt/models/marts/organisations_dihal.sql
+++ dbt/models/marts/organisations_dihal.sql
@@ -1,3 +1,4 @@
-select {{ dbt_utils.star(source('emplois', 'organisations')) }}
+select
+	{{ dbt_utils.star(source('emplois', 'organisations')) }}
 from {{ source('emplois', 'organisations') }}
 where type in ('CHRS', 'CHU', 'RS_FJT', 'OIL')
