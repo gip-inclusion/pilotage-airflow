c78864f2d8a8b9b0f75040bb461dee63a69b2e8d
laurinehu <laurinehu@gmail.com>
2023-05-11 12:34:02 +0200

    no idea why this did pass sqlfluff ;p

--- dbt/models/marts/candidatures_dihal.sql
+++ dbt/models/marts/candidatures_dihal.sql
@@ -1,3 +1,3 @@
 select {{ dbt_utils.star(ref('candidatures_echelle_locale')) }}
-from ref('candidatures_echelle_locale')
+from {{ ref('candidatures_echelle_locale') }}
 where 'type_org_prescripteur' in ('CHRS', 'CHU', 'RS_FJT', 'OIL')
