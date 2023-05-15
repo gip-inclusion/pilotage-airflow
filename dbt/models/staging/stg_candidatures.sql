0c9e894f2bec85c48d28ef97683b2a9b54aa919b
laurinehu <laurinehu@gmail.com>
2023-05-15 18:39:30 +0200

    minor fix to use dbt_utils.star properly

--- dbt/models/staging/stg_candidatures.sql
+++ dbt/models/staging/stg_candidatures.sql
@@ -1,21 +1,18 @@
 select
     {% if env_var('CI', '') %}
-        *
+        id,
     {% else %}
-        {{ dbt_utils.star(source('emplois', 'candidatures'), except=["état", "origine", "délai_de_réponse", "délai_prise_en_compte", "cdd_id_org_prescripteur", "cdd_id_structure"]) }},
-        -- laurine: je mets tout ici car sinon dbt râle car je lui demande de créer des colonnes qui existent déjà
-        id_structure                              as cdd_id_structure,
-        id_org_prescripteur                       as cdd_id_org_prescripteur,
-        case
-            when "état" = 'Candidature déclinée' then 'Candidature refusée'
-            else "état"
-        end                                       as "état",
-        case
-            when origine = 'Candidat' then 'Candidature en ligne'
-            else origine
-        end                                       as origine,
-        extract(day from "délai_de_réponse")      as temps_de_reponse,
-        extract(day from "délai_prise_en_compte") as temps_de_prise_en_compte
+        {{ dbt_utils.star(source('emplois', 'candidatures'), except=["état", "origine", "délai_de_réponse", "délai_prise_en_compte"]) }},
     {% endif %}
+    case
+        when "état" = 'Candidature déclinée' then 'Candidature refusée'
+        else "état"
+    end                                       as "état",
+    case
+        when origine = 'Candidat' then 'Candidature en ligne'
+        else origine
+    end                                       as origine,
+    extract(day from "délai_de_réponse")      as temps_de_reponse,
+    extract(day from "délai_prise_en_compte") as temps_de_prise_en_compte
 from
     {{ source('emplois', 'candidatures') }}
