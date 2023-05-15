ddc19f6c8dbd69875503795d4f2d51ebb4447522
laurinehu <laurinehu@gmail.com>
2023-05-15 17:29:41 +0200

    simplif with dbt_star utils, thx yannick

--- dbt/models/staging/stg_candidatures.sql
+++ dbt/models/staging/stg_candidatures.sql
@@ -1,34 +1,21 @@
 select
-    id,
-    "id_anonymisé",
-    id_candidat,
-    "id_candidat_anonymisé",
-    date_candidature,
-    date_embauche,
-    "délai_de_réponse",
-    "délai_prise_en_compte",
-    "département_structure",
-    safir_org_prescripteur,
-    id_structure                              as cdd_id_structure,
-    id_org_prescripteur                       as cdd_id_org_prescripteur,
-    motif_de_refus,
-    "nom_département_structure",
-    "région_structure",
-    nom_structure,
-    type_structure,
-    "origine_détaillée",
-    nom_org_prescripteur,
-    "nom_prénom_conseiller",
-    injection_ai,
-    case
-        when "état" = 'Candidature déclinée' then 'Candidature refusée'
-        else "état"
-    end                                       as "état",
-    case
-        when origine = 'Candidat' then 'Candidature en ligne'
-        else origine
-    end                                       as origine,
-    extract(day from "délai_de_réponse")      as temps_de_reponse,
-    extract(day from "délai_prise_en_compte") as temps_de_prise_en_compte
+    {% if env_var('CI', ',') %}
+        *
+    {% else %}
+        {{ dbt_utils.star(source('emplois', 'candidatures'), except=["état", "origine", "délai_de_réponse", "délai_prise_en_compte", "cdd_id_org_prescripteur", "cdd_id_structure"]) }},
+        -- laurine: je mets tout ici car sinon dbt râle car je lui demande de créer des colonnes qui existent déjà
+        id_structure                              as cdd_id_structure,
+        id_org_prescripteur                       as cdd_id_org_prescripteur,
+        case
+            when "état" = 'Candidature déclinée' then 'Candidature refusée'
+            else "état"
+        end                                       as "état",
+        case
+            when origine = 'Candidat' then 'Candidature en ligne'
+            else origine
+        end                                       as origine,
+        extract(day from "délai_de_réponse")      as temps_de_reponse,
+        extract(day from "délai_prise_en_compte") as temps_de_prise_en_compte
+    {% endif %}
 from
     {{ source('emplois', 'candidatures') }}
