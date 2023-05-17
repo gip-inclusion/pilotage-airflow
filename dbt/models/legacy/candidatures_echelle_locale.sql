8e465e39fd15342784a46fc744b451f942fa04a3
YannickPassa <yannick.passarelli@gmail.com>
2023-05-17 17:39:02 +0200

    network's staging tables

--- dbt/models/legacy/candidatures_echelle_locale.sql
+++ dbt/models/legacy/candidatures_echelle_locale.sql
@@ -1,58 +1,3 @@
-/*On créé une colonne par réseau au cas ou une même structure appartienne à différents réseaux */
-with adherents_coorace as (
-    select distinct
-        ria."Réseau IAE" as reseau_coorace,
-        s.id             as id_structure,
-        (ria."SIRET")    as siret
-    from {{ source('oneshot', 'reseau_iae_adherents') }} as ria
-    inner join structures as s
-        on s.siret = ria."SIRET"
-    where ria."Réseau IAE" = 'Coorace'
-),
-
-adherents_fei as (
-    select distinct
-        ria."Réseau IAE" as reseau_fei,
-        s.id             as id_structure,
-        (ria."SIRET")    as siret
-    from {{ source('oneshot', 'reseau_iae_adherents') }} as ria
-    inner join structures as s
-        on s.siret = ria."SIRET"
-    where ria."Réseau IAE" = 'FEI'
-),
-
-adherents_emmaus as (
-    select distinct
-        ria."Réseau IAE" as reseau_emmaus,
-        s.id             as id_structure,
-        (ria."SIRET")    as siret
-    from {{ source('oneshot', 'reseau_iae_adherents') }} as ria
-    inner join structures as s
-        on s.siret = ria."SIRET"
-    where ria."Réseau IAE" = 'Emmaus'
-),
-
-adherents_unai as (
-    select distinct
-        ria."Réseau IAE" as reseau_unai,
-        s.id             as id_structure,
-        (ria."SIRET")    as siret
-    from {{ source('oneshot', 'reseau_iae_adherents') }} as ria
-    inner join structures as s
-        on s.siret = ria."SIRET"
-    where ria."Réseau IAE" = 'Unai'
-),
-
-adherents_cocagne as (
-    select distinct
-        ria."Réseau IAE" as reseau_cocagne,
-        s.id             as id_structure,
-        (ria."SIRET")    as siret
-    from {{ source('oneshot', 'reseau_iae_adherents') }} as ria
-    inner join structures as s
-        on s.siret = ria."SIRET"
-    where ria."Réseau IAE" = 'Cocagne'
-)
 select
     {% if env_var('CI', '') %}
         id,
@@ -62,40 +7,20 @@ select
         {{ dbt_utils.star(ref('stg_bassin_emploi'), except=["nom_departement","nom_region","type_epci","id_structure"] ,relation_alias='bassin_emploi') }},
         {{ dbt_utils.star(ref('stg_reseaux'), except=["SIRET","id_structure"] ,relation_alias='rsx') }},
     {% endif %}
-    nom_org.type_auteur_diagnostic_detaille,
-    case
-        when adherents_emmaus.reseau_emmaus = 'Emmaus' then 'Oui'
-        else 'Non'
-    end as reseau_emmaus,
-    case
-        when adherents_coorace.reseau_coorace = 'Coorace' then 'Oui'
-        else 'Non'
-    end as reseau_coorace,
-    case
-        when adherents_fei.reseau_fei = 'FEI' then 'Oui'
-        else 'Non'
-    end as reseau_fei,
-    case
-        when adherents_unai.reseau_unai = 'Unai' then 'Oui'
-        else 'Non'
-    end as reseau_unai,
-    case
-        when adherents_cocagne.reseau_cocagne = 'Cocagne' then 'Oui'
-        else 'Non'
-    end as reseau_cocagne
+    nom_org.type_auteur_diagnostic_detaille
 from
     {{ ref('stg_candidatures') }} as candidatures
 left join {{ ref('stg_bassin_emploi') }} as bassin_emploi
     on bassin_emploi.id_structure = candidatures.id_structure
-left join adherents_emmaus
+left join {{ ref('stg_emmaus') }} as adherents_emmaus
     on adherents_emmaus.id_structure = candidatures.id_structure
-left join adherents_coorace
+left join {{ ref('stg_coorace') }} as adherents_coorace
     on adherents_coorace.id_structure = candidatures.id_structure
-left join adherents_fei
+left join {{ ref('stg_fei') }} as adherents_fei
     on adherents_fei.id_structure = candidatures.id_structure
-left join adherents_unai
+left join {{ ref('stg_unai') }} as adherents_unai
     on adherents_unai.id_structure = candidatures.id_structure
-left join adherents_cocagne
+left join {{ ref('stg_cocagne') }} as adherents_cocagne
     on adherents_cocagne.id_structure = candidatures.id_structure
 left join {{ ref('stg_org_prescripteur') }} as org_prescripteur
     on org_prescripteur.id_org = candidatures.id_org_prescripteur
---
+++ dbt/models/staging/stg_cocagne.sql
@@ -0,0 +1,8 @@
+select distinct
+    ria."Réseau IAE" as reseau_cocagne,
+    s.id             as id_structure,
+    (ria."SIRET")    as siret
+from {{ source('oneshot', 'reseau_iae_adherents') }} as ria
+inner join {{ source('emplois', 'structures') }} as s
+    on s.siret = ria."SIRET"
+where ria."Réseau IAE" = 'Cocagne'
