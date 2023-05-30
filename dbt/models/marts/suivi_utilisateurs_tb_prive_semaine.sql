000c282dd4dbb30c5fc78c1afb3ce17333e1fa04
laurinehu <laurinehu@gmail.com>
2023-05-30 17:45:19 +0200

    add visitor informations on suivi_utilisateurs_table

--- dbt/models/marts/suivi_utilisateurs_tb_prive_semaine.sql
+++ dbt/models/marts/suivi_utilisateurs_tb_prive_semaine.sql
@@ -1,20 +1,33 @@
 select
-    visits.user_id                        as id_utilisateur,
-    visits.user_kind                      as type_utilisateur,
-    visits.dashboard_id                   as id_tb,
-    metabase_ids.nom_tb                   as nom_tb,
-    visits.department                     as departement,
-    visits.region                         as region,
-    date_part('week', visits.measured_at) as semaine,
-    count(distinct visits.measured_at)    as nb_visites
+    visits.user_id                                                   as id_utilisateur,
+    metabase_ids.nom_tb                                              as nom_tb,
+    visits.department                                                as departement,
+    visits.region                                                    as region,
+    case
+        when visits.user_kind = 'prescriber' then 'prescripteur'
+        when visits.user_kind = 'siae_staff' then 'siae'
+        when visits.user_kind = 'labor_inspector' then 'institution'
+        when visits.user_kind = 'itou_staff' then 'staff interne'
+    end                                                              as type_utilisateur,
+    coalesce(organisations.type, structures.type, institutions.type) as type_organisation,
+    coalesce(organisations.nom, structures.nom, institutions.nom)    as nom_organisation,
+    date_part('week', visits.measured_at)                            as semaine,
+    count(distinct visits.measured_at)                               as nb_visites
 from {{ source('emplois', 'c1_private_dashboard_visits_v0') }} as visits
 left join {{ ref('metabase_dashboards') }} as metabase_ids
     on metabase_ids.id_tb = cast(visits.dashboard_id as integer)
+left join {{ source('emplois', 'organisations') }} as organisations
+    on organisations.id = cast(visits.current_prescriber_organization_id as integer) and visits.user_kind = 'prescriber'
+left join {{ source('emplois', 'structures') }} as structures
+    on structures.id = cast(visits.current_siae_id as integer) and visits.user_kind = 'siae_staff'
+left join {{ source('emplois', 'institutions') }} as institutions
+    on institutions.id = cast(visits.current_institution_id as integer) and visits.user_kind = 'labor_inspector'
 group by
-    visits.user_id,
-    visits.user_kind,
-    visits.dashboard_id,
+    id_utilisateur,
     nom_tb,
-    visits.department,
+    departement,
     region,
+    type_utilisateur,
+    type_organisation,
+    nom_organisation,
     semaine
