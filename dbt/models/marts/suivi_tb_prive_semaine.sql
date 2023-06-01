cc973114af55fb9d28d80e9c7506154bdba275eb
laurinehu <laurinehu@gmail.com>
2023-06-01 09:33:07 +0200

    truncate date to agregate by week (issue) + rename visits to visites in suivi_tb_prive

--- dbt/models/marts/suivi_tb_prive_semaine.sql
+++ dbt/models/marts/suivi_tb_prive_semaine.sql
@@ -1,17 +1,17 @@
 select
-    id_tb,
     nom_tb,
     semaine,
+    num_semaine,
     -- nb total d'utilisateurs cette semaine
     count(id_utilisateur) as nb_utilisateurs,
-    -- est-ce que l'utilisateur est revenu plus d'une fois cette semaine
+    -- nb d'utilisateurs revenus au moins une fois cette semaine
     count(
         case
-            when nb_visits > 1 then 1
+            when nb_visites > 1 then 1
         end
     )                     as nb_utilisateurs_plusieurs_visites
 from {{ ref('suivi_utilisateurs_tb_prive_semaine') }}
 group by
-    id_tb,
     nom_tb,
-    semaine
+    semaine,
+    num_semaine
