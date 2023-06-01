cc4e6a0e4f21f5d5c7d19edf1aac870d63a8c135
laurinehu <laurinehu@gmail.com>
2023-06-01 09:33:07 +0200

    truncate date to agregate by week (issue) + rename visits to visites in suivi_tb_prive

--- dbt/models/marts/suivi_utilisateurs_tb_prive_semaine.sql
+++ dbt/models/marts/suivi_utilisateurs_tb_prive_semaine.sql
@@ -1,19 +1,20 @@
 select
-    visits.user_id                                                                       as id_utilisateur,
-    metabase_ids.nom_tb                                                                  as nom_tb,
-    visits.department                                                                    as departement,
-    visits.region                                                                        as region,
+    visits.user_id                                                                                          as id_utilisateur,
+    metabase_ids.nom_tb                                                                                     as nom_tb,
+    visits.department                                                                                       as departement,
+    visits.region                                                                                           as region,
     case
         when visits.user_kind = 'prescriber' then 'prescripteur'
         when visits.user_kind = 'siae_staff' then 'siae'
         when visits.user_kind = 'labor_inspector' then 'institution'
         when visits.user_kind = 'itou_staff' then 'staff interne'
-    end                                                                                  as type_utilisateur,
-    coalesce(organisations.type, structures.type, institutions.type)                     as type_organisation,
-    coalesce(organisations.nom, structures.nom, institutions.nom)                        as nom_organisation,
-    visits.measured_at - INTERVAL '1 day' * (extract('dow' from visits.measured_at) - 1) as semaine,
-    date_part('week', visits.measured_at)                                                as num_semaine,
-    count(distinct visits.measured_at)                                                   as nb_visites
+    end                                                                                                     as type_utilisateur,
+    coalesce(organisations.type, structures.type, institutions.type)                                        as type_organisation,
+    coalesce(organisations.nom, structures.nom, institutions.nom)                                           as nom_organisation,
+    -- recover date of the monday of the current week
+    date_trunc('day', visits.measured_at) - INTERVAL '1 day' * (extract('dow' from visits.measured_at) - 1) as semaine,
+    date_part('week', visits.measured_at)                                                                   as num_semaine,
+    count(distinct visits.measured_at)                                                                      as nb_visites
 from {{ source('emplois', 'c1_private_dashboard_visits_v0') }} as visits
 left join {{ ref('metabase_dashboards') }} as metabase_ids
     on metabase_ids.id_tb = cast(visits.dashboard_id as INTEGER)
