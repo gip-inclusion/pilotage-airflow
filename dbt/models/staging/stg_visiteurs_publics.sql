e10c99ddd7ccf31d4647da3634328b13ab0c130c
YannickPassa <yannick.passarelli@gmail.com>
2023-06-07 11:37:29 +0200

    refacto of the table following the unique visitors gathered from matomo

---
+++ dbt/models/staging/stg_visiteurs_publics.sql
@@ -0,0 +1,10 @@
+select
+    vp."Tableau de bord"                    as tableau_de_bord,
+    'TB public'                             as type_de_tb,
+    null                                    as "département",
+    null                                    as "nom_département",
+    to_date(vp."Date", 'YYYY-MM-DD')        as semaine,
+    to_number(vp."Unique visitors", '9999') as visiteurs_uniques
+from
+    /* Nouvelle table créée par Victor qui reprend toutes les infos des visiteurs des TBs publics */
+    {{ source('matomo', 'suivi_visiteurs_tb_publics_v1') }} as vp
