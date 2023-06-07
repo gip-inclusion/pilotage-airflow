5a874625db1cf4aa1159b66cfd4358f17962e200
laurinehu <laurinehu@gmail.com>
2023-06-07 15:06:48 +0200

    recover region by joining with insee to solve missing regions

--- dbt/models/marts/oneshot/atigip_2022.sql
+++ dbt/models/marts/oneshot/atigip_2022.sql
@@ -1,16 +1,19 @@
 select
-    org."région"                                                         as region_conseiller,
-    org."département"                                                    as dept_conseiller,
-    org.code_postal                                                      as cp_conseiller,
-    org.nom                                                              as nom_spip,
-    count(cel.id)                                                        as nb_orientations,
-    sum(case when cel."état" = 'Candidature acceptée' then 1 else 0 end) as "nb_candidatures_acceptées",
-    sum(case when cel."état" = 'Candidature refusée' then 1 else 0 end)  as "nb_candidatures_refusées"
+    insee_regions."LIBELLE"                                                                                       as "Région conseiller",
+    org."département"                                                                                             as "Département conseiller",
+    org.code_postal                                                                                               as "Code postal conseiller",
+    org.nom                                                                                                       as "Nom SPIP",
+    count(cel.id)                                                                                                 as "Nombre d'orientations",
+    sum(case when cel."état" = 'Candidature acceptée' then 1 else 0 end)                                          as "Nombre de candidatures acceptées",
+    sum(case when cel."état" = 'Candidature refusée' then 1 else 0 end)                                           as "Nombre de candidatures refusées",
+    sum(case when cel."état" != 'Candidature refusée' and cel."état" != 'Candidature acceptée' then 1 else 0 end) as "Nombre de candidatures en cours"
 from {{ ref('candidatures_echelle_locale') }} as cel left join {{ ref('stg_organisations') }} as org on cel.id_org_prescripteur = org.id
+left join insee_departements on ltrim(org."département", '0') = insee_departements."DEP"
+left join insee_regions on insee_departements."REG" = insee_regions."REG"
 where date_part('year', cel.date_candidature) = 2022 and org.type = 'SPIP'
 group by
     cel."nom_prénom_conseiller",
-    org."région",
+    insee_regions."LIBELLE",
     org."département",
     org.code_postal,
     org.nom
