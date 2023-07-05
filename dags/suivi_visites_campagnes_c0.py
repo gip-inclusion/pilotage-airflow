e6c0e0fb1d647750172155decca3e01ef2522d7b
laurinehu <laurinehu@gmail.com>
2023-07-05 18:15:03 +0200

    dag to extract matomo visits per campaign

---
+++ dags/suivi_visites_campagnes_c0.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from airflow.decorators import task
+from airflow.models import Variable
+from airflow.operators import empty
+
+from dags.common import db, default_dag_args, matomo, slack
+
+
+with DAG(
+    "suivi_visites_campagnes_c0",
+    schedule_interval="@monthly",
+    **default_dag_args(),
+) as dag:
+    start = empty.EmptyOperator(task_id="start")
+
+    end = slack.success_notifying_task()
+
+    @task(task_id="get_visits_per_campaign")
+    def get_visits_per_campaign(**kwargs):
+        matomo_base_url = Variable.get("MATOMO_BASE_URL")
+        tok = Variable.get("GIP_MATOMO_TOKEN")
+        out_dtf = matomo.get_visits_per_campaign_from_matomo(matomo_base_url, tok)
+        out_dtf.to_sql("visits_per_campaign_c0", con=db.connection_engine(), if_exists="replace", index=False)
+
+    visits_per_campaign = get_visits_per_campaign()
+
+    (start >> visits_per_campaign >> end)
