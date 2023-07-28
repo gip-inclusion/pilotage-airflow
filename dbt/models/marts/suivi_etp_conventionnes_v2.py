49142a7366b3a99ec843a4176c15d1d69224274b
Victor Perron <victor@iso3103.net>
2023-07-28 18:01:06 +0200

    suivi_etp_conventionnes_v2: Remove a useless intermediate function

--- dbt/models/marts/suivi_etp_conventionnes_v2.py
+++ dbt/models/marts/suivi_etp_conventionnes_v2.py
@@ -1,4 +1,6 @@
-def computing_etp_part(df):
+def model(dbt, session):
+    df = dbt.ref("stg_etp_conventionnes")
+
     df["Montant_total_aide"] = (
         df["effectif_mensuel_conventionné"] * df["af_montant_unitaire_annuel_valeur"] / 12 * df["duree_annexe"]
     )
@@ -11,14 +13,10 @@ def computing_etp_part(df):
     df["etp_conventionnes_etat"] = (df["effectif_mensuel_conventionné"] * (1 - df["part_conventionnement_cd"])) * (
         df["duree_annexe"] / 12
     )
+
     # Nombre de brsa pouvant être subventionnés avec le montant cofinancé (par mois)
     df["nb_brsa_cible_mensuel"] = (df["af_mt_cofinance"] / df["duree_annexe"]) / (0.88 * df["montant_rsa"])
     # idem par an
     df["nb_brsa_cible_annuel"] = df["af_mt_cofinance"] / (0.88 * df["montant_rsa"])
-    return df
 
-
-def model(dbt, session):
-    df = dbt.ref("stg_etp_conventionnes")
-    df = computing_etp_part(df)
     return df
