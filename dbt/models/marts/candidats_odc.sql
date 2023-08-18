7de05c75ba13f1d3d86f05f25b41fe482ca4eefa
laurinehu <laurinehu@gmail.com>
2023-08-18 12:10:00 +0200

    forgot to commit candidatures

--- dbt/models/marts/candidats_odc.sql
+++ dbt/models/marts/candidats_odc.sql
@@ -2,7 +2,7 @@ select distinct
     {{ pilo_star(source('emplois', 'candidats'), relation_alias='cdd') }},
     candidatures_odc.dept_org_prescripteur,
     candidatures_odc.nom_dept_org_prescripteur,
-    candidatures_odc.region_dept_org_prescripteur,
+    candidatures_odc.region_org_prescripteur,
     candidatures_odc.nom_org_prescripteur
 from {{ source('emplois', 'candidats') }} as cdd
 inner join
--- dbt/models/marts/candidatures_odc.sql
+++ dbt/models/marts/candidatures_odc.sql
@@ -1,4 +1,8 @@
-select {{ pilo_star(ref('candidatures_echelle_locale'), relation_alias="cel") }}
+select
+    {{ pilo_star(ref('candidatures_echelle_locale'), relation_alias="cel") }},
+    prescripteurs_odc."région"          as region_org_prescripteur,
+    prescripteurs_odc."nom_département" as nom_dept_org_prescripteur,
+    prescripteurs_odc."département"     as dept_org_prescripteur
 from {{ ref('candidatures_echelle_locale') }} as cel
 inner join
     {{ ref('stg_prescripteurs_odc') }} as prescripteurs_odc
