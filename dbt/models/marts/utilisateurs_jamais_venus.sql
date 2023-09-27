83e7e24bc167ace1336d0554cf44b80a95168089
laurinehu <laurinehu@gmail.com>
2023-09-27 17:19:18 +0200

    add table for following if c1 users came to c2 private tb

---
+++ dbt/models/marts/utilisateurs_jamais_venus.sql
@@ -0,0 +1,19 @@
+select
+    utilisateurs_all.email,
+    utilisateurs_all.type,
+    case
+        when array_agg(nom_tb) = array[null] then null
+        else array_agg(nom_tb)
+    end as tb_visited,
+    case
+        when array_agg(nom_tb) = array[null] then 0
+        else array_length(array_agg(nom_tb), 1)
+    end as nb_tb_visited
+from
+    {{ source('emplois', 'utilisateurs') }} as utilisateurs_all
+left join
+    {{ ref('suivi_utilisateurs_tb_prive_semaine') }} as utilisateurs_tb_prives
+    on utilisateurs_tb_prives.email_utilisateur = utilisateurs_all.email
+group by
+    utilisateurs_all.email,
+    utilisateurs_all.type
