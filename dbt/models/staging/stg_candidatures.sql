b26f4a4973f997c9e855169f2346521ddb36e1af
laurinehu <laurinehu@gmail.com>
2023-10-24 16:31:13 +0200

    solve minor typos

--- dbt/models/staging/stg_candidatures.sql
+++ dbt/models/staging/stg_candidatures.sql
@@ -3,7 +3,7 @@ select
         except=["id", "date_mise_à_jour_metabase", "état", "motif_de_refus", "origine", "délai_de_réponse", "délai_prise_en_compte", "origine_détaillée"]) }},
     {{ pilo_star(ref('stg_organisations'), except=["id", "date_mise_à_jour_metabase", "ville", "code_commune", "type", "date_inscription", "total_candidatures", "total_membres", "total_embauches", "date_derniere_candidature"], relation_alias='org_prescripteur') }},
     candidatures.id                                        as id,
-    struct.bassin_emploi_structure                         as bassin_emploi_structure,
+    struct.bassin_d_emploi                                 as bassin_emploi_structure,
     org_prescripteur.zone_emploi                           as bassin_emploi_prescripteur,
     org_prescripteur.type                                  as type_org_prescripteur,
     org_prescripteur.date_inscription                      as date_inscription_orga,
