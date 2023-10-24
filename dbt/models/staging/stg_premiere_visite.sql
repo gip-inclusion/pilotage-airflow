86cb18579a16f9df33f2f89c4f8d25d93b8e52c3
YannickPassa <yannick.passarelli@gmail.com>
2023-10-24 15:09:14 +0200

    addition of the  dashboard information

--- dbt/models/staging/stg_premiere_visite.sql
+++ dbt/models/staging/stg_premiere_visite.sql
@@ -1,5 +1,8 @@
 select
     user_id,
+    dashboard_id,
     min(measured_at) as premiere_visite
 from {{ source('emplois', 'c1_private_dashboard_visits_v0') }}
-group by user_id
+group by
+    user_id,
+    dashboard_id
