5fd3886e84802d84fafacca3998bd0ec4d97be80
laurinehu <laurinehu@gmail.com>
2023-10-31 14:35:57 +0100

    move from idx to marts

--- dbt/models/marts/recrutements.sql
+++ dbt/models/marts/recrutements.sql
@@ -1,12 +1,5 @@
-{{ config(
-    materialized = 'table',
-    indexes=[
-      {'columns': ['contrat_id_ctr', 'contrat_id_pph'], 'type' : 'btree', 'unique' : False},
-    ]
- ) }}
-
 select
-    ctr.contrat_id_ctr,
+    ctr.contrat_id_ctr                                                    as identifiant_salarie,
     ctr.contrat_id_structure,
     ctr.contrat_id_pph,
     ctr.contrat_date_embauche,
@@ -16,8 +9,8 @@ select
     af.af_numero_annexe_financiere,
     salarie.genre_salarie,
     salarie.tranche_age,
-    salarie.qpv,
-    salarie.zrr,
+    salarie.qpv                                                           as qpv,
+    salarie.zrr                                                           as zrr,
     rnf.rnf_libelle_niveau_form_empl                                      as niveau_formation_salarie,
     struct.structure_denomination,
     struct.structure_adresse_admin_commune,
@@ -27,17 +20,18 @@ select
     struct.nom_epci_structure,
     af.nom_departement_af,
     af.nom_region_af,
+    ref_disp.type_structure_emplois                                       as type_siae,
     case
         when ctr.contrat_salarie_rqth then 'OUI'
         else 'NON'
-    end                                                                   as salarie_rqth,
+    end                                                                   as rqth,
     case
         when ctr.contrat_salarie_rsa = 'OUI-M' then 'OUI'
         when ctr.contrat_salarie_rsa = 'OUI-NM' then 'OUI'
         else 'NON'
-    end                                                                   as salarie_brsa,
-    split_part(ctr.contrat_mesure_disp_code, '_', 1)                      as type_structure_entree,
-    date_trunc('month', to_date(ctr.contrat_date_embauche, 'DD/MM/YYYY')) as mois_entree
+    end                                                                   as brsa,
+    date_trunc('month', to_date(ctr.contrat_date_embauche, 'DD/MM/YYYY')) as mois_recrutement,
+    date_trunc('year', to_date(ctr.contrat_date_embauche, 'DD/MM/YYYY'))  as annee_recrutement
 from {{ ref('fluxIAE_ContratMission_v2') }} as ctr
 left join {{ ref('fluxIAE_EtatMensuelIndiv_v2') }} as emi
     on ctr.contrat_id_pph = emi.emi_pph_id and ctr.contrat_id_ctr = emi.emi_ctr_id
@@ -49,6 +43,8 @@ left join {{ source('fluxIAE', 'fluxIAE_RefNiveauFormation') }} as rnf
     on ctr.contrat_niveau_de_formation_code = rnf.rnf_id
 left join {{ ref('fluxIAE_Structure_v2') }} as struct
     on ctr.contrat_id_structure = struct.structure_id_siae
+left join {{ ref('ref_mesure_dispositif_asp') }} as ref_disp
+    on ctr.contrat_mesure_disp_code = ref_disp.af_mesure_dispositif_code
 where
     ctr.contrat_type_contrat = 0
     and af.af_etat_annexe_financiere_code in (
