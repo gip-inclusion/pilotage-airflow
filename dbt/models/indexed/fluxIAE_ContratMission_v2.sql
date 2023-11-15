0682aa4b40a66d76c3cb57565d6137b4ef5d2782
laurinehu <laurinehu@gmail.com>
2023-11-15 11:13:36 +0100

    add a staging contract table with a column specifying recrutement id

--- dbt/models/indexed/fluxIAE_ContratMission_v2.sql
+++ dbt/models/indexed/fluxIAE_ContratMission_v2.sql
@@ -6,6 +6,8 @@
  ) }}
 
 select
-    {{ pilo_star(source('fluxIAE', 'fluxIAE_ContratMission')) }}
+    {{ pilo_star(source('fluxIAE', 'fluxIAE_ContratMission'), except=['contrat_date_embauche', "contrat_date_fin_contrat"]) }},
+    to_date(contrat_date_embauche, 'DD/MM/YYYY')    as contrat_date_embauche,
+    to_date(contrat_date_fin_contrat, 'DD/MM/YYYY') as contrat_date_fin_contrat
 from
     {{ source('fluxIAE', 'fluxIAE_ContratMission') }}
