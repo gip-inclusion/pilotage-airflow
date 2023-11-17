db3e67a232da30c8f5503a430012e33f01083b25
laurinehu <laurinehu@gmail.com>
2023-11-17 12:03:23 +0100

    calculation of contracts with their reconductions

--- dbt/models/staging/stg_contrats.sql
+++ dbt/models/staging/stg_contrats.sql
@@ -1,7 +1,13 @@
-select
-    {{ pilo_star(ref('fluxIAE_ContratMission_v2')) }},
+select distinct
+    ctr.contrat_id_pph,
+    ctr.contrat_id_ctr,
+    ctr.contrat_date_embauche,
+    ctr.contrat_date_fin_contrat,
+    ctr.contrat_type_contrat,
     sum(case
-        when contrat_type_contrat = 0 then 1
+        when ctr.contrat_type_contrat = 0 then 1
         else 0
-    end) over (partition by contrat_id_pph order by contrat_date_embauche) as id_recrutement
-from {{ ref('fluxIAE_ContratMission_v2') }}
+    end) over (partition by ctr.contrat_id_pph order by contrat_date_embauche) as id_recrutement
+from {{ ref('fluxIAE_EtatMensuelIndiv_v2') }} as emi
+left join {{ ref('fluxIAE_ContratMission_v2') }} as ctr
+    on ctr.contrat_id_ctr = emi.emi_ctr_id
--- dbt/models/staging/stg_recrutements.sql
+++ dbt/models/staging/stg_recrutements.sql
@@ -1,6 +1,7 @@
 select
     contrat_id_pph,
     min(contrat_id_ctr)           as id_premier_contrat,
+    max(contrat_id_ctr)           as id_derniere_reconduction,
     min(contrat_date_embauche)    as date_recrutement,
     max(contrat_date_fin_contrat) as date_fin_recrutement,
     max(contrat_type_contrat)     as nb_reconductions
