386ac3c41f9941de1fe6273699603e810a5326fc
YannickPassa <yannick.passarelli@gmail.com>
2023-11-27 14:46:01 +0100

    singular test
    typo

--- dbt/models/staging/stg_sum_etp.sql
+++
@@ -1,9 +0,0 @@
-select
-    (
-        select round(sum(nombre_etp_consommes_reels_mensuels))
-        from {{ ref("suivi_realisation_convention_mensuelle") }}
-    ) as etp_somme_tous,
-    (
-        select round(sum(somme_etp_realises))
-        from {{ ref("suivi_realisation_convention_par_structure") }}
-    ) as etp_somme_strct
---
+++ dbt/tests/test_sum_etp.sql
@@ -0,0 +1,19 @@
+{{ config(
+    severity = 'warn',
+    warn_if = '>1') }}
+
+with sum_etp as (
+    select
+        (
+            select round(sum(nombre_etp_consommes_reels_mensuels))
+            from {{ ref("suivi_realisation_convention_mensuelle") }}
+        ) as etp_somme_tous,
+        (
+            select round(sum(somme_etp_realises))
+            from {{ ref("suivi_realisation_convention_par_structure") }}
+        ) as etp_somme_strct
+)
+
+select *
+from sum_etp
+where etp_somme_tous = etp_somme_strct
