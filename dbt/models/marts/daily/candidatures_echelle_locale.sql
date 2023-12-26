196e32fe7f12b42990eb636e7b0c75344abe0dab
laurinehu <laurinehu@gmail.com>
2023-12-26 16:45:24 +0100

    replace null tranche_age and null genre by 'Non renseigné'

--- dbt/models/marts/daily/candidatures_echelle_locale.sql
+++ dbt/models/marts/daily/candidatures_echelle_locale.sql
@@ -3,20 +3,20 @@ select
     case
         when candidatures.injection_ai = 0 then 'Non'
         else 'Oui'
-    end                                   as reprise_de_stock_ai,
+    end                                                 as reprise_de_stock_ai,
     case
         when candidatures.type_org_prescripteur = org.code
             then org.label
         else candidatures."origine_détaillée"
-    end                                   as "origine_détaillée",
-    candidats.sous_type_auteur_diagnostic as auteur_diag_candidat_detaille,
-    candidats.type_auteur_diagnostic      as auteur_diag_candidat,
+    end                                                 as "origine_détaillée",
+    coalesce(candidats.tranche_age, 'Non renseigné')    as tranche_age,
+    coalesce(candidats.sexe_selon_nir, 'Non renseigné') as genre_candidat,
+    candidats.sous_type_auteur_diagnostic               as auteur_diag_candidat_detaille,
+    candidats.type_auteur_diagnostic                    as auteur_diag_candidat,
     candidats.eligibilite_dispositif,
-    candidats.tranche_age,
     candidats.eligible_cej,
-    candidats.sexe_selon_nir              as genre_candidat,
     candidats.eligible_cdi_inclusion,
-    candidats.date_inscription            as date_inscription_candidat
+    candidats.date_inscription                          as date_inscription_candidat
 from
     {{ ref('stg_candidatures') }} as candidatures
 left join {{ ref('candidats') }} as candidats
