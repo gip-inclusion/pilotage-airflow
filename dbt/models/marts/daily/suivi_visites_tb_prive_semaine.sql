3896bfa7368ee3c9a5d91450f88272145a070483
laurinehu <laurinehu@gmail.com>
2023-12-28 18:20:30 +0100

    solve nb_visits calculation

--- dbt/models/marts/daily/suivi_visites_tb_prive_semaine.sql
+++ dbt/models/marts/daily/suivi_visites_tb_prive_semaine.sql
@@ -6,19 +6,23 @@ select
     visits.nom_tb,
     visits.semaine,
     visits.num_semaine,
+    -- nb utilisateurs revenus plusieurs fois cette semaine
+    nb_revenus.nb_utilisateurs_plusieurs_visites,
     -- mail des utilisateurs venus cette semaine
     array_agg(distinct c1_users.email) as liste_utilisateurs,
     -- nb total d'utilisateurs cette semaine
-    count(visits.id_utilisateur)       as nb_utilisateurs,
-    -- nb d'utilisateurs revenus au moins une fois cette semaine
-    count(
-        case
-            when visits.nb_visites > 1 then 1
-        end
-    )                                  as nb_utilisateurs_plusieurs_visites
+    count(visits.id_utilisateur)       as nb_utilisateurs
 from {{ ref('suivi_utilisateurs_tb_prive_semaine') }} as visits
 left join {{ source('emplois', 'utilisateurs') }} as c1_users
-    on c1_users.id = cast(visits.id_utilisateur as INTEGER)
+    on c1_users.id = cast(visits.id_utilisateur as integer)
+left join {{ ref('stg_nb_utilisateurs_revenus_semaine') }} as nb_revenus
+    on
+        nb_revenus.num_semaine = cast(visits.num_semaine as integer)
+        and nb_revenus.nom_tb = visits.nom_tb
+        and nb_revenus.region = visits.region
+        and nb_revenus.departement = visits.departement
+        and nb_revenus.type_utilisateur = visits.type_utilisateur
+        and nb_revenus.type_organisation = visits.type_organisation
 group by
     visits.region,
     visits.departement,
@@ -26,4 +30,5 @@ group by
     visits.type_organisation,
     visits.num_semaine,
     visits.nom_tb,
-    visits.semaine
+    visits.semaine,
+    nb_revenus.nb_utilisateurs_plusieurs_visites
