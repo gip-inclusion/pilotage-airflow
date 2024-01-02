f1109d6c8214a87a0783f6bf8de94bcccbd59415
laurinehu <laurinehu@gmail.com>
2024-01-02 19:40:04 +0100

    optimize following dejafaits remarks

--- dbt/models/marts/daily/taux_couverture.sql
+++ dbt/models/marts/daily/taux_couverture.sql
@@ -1,5 +1,6 @@
 select
-    visites.type,
+    visites.semaine,
+    visites.type_utilisateur,
     visites.profil,
     visites.nom_tb,
     visites."département_num",
@@ -13,12 +14,13 @@ select
 from {{ ref('suivi_visites_tb_prive_semaine') }} as visites
 left join {{ ref('nb_utilisateurs_potentiels') }} as potentiel
     on
-        visites.type = potentiel.type
+        visites.type_utilisateur = potentiel.type_utilisateur
         and visites.profil = potentiel.profil
         and visites."département_num" = potentiel."département_num"
         and visites."région" = potentiel."région"
 group by
-    visites.type,
+    visites.semaine,
+    visites.type_utilisateur,
     visites.profil,
     visites.nom_tb,
     visites."département_num",
