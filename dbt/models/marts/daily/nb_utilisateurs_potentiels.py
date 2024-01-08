36355ed18a6f7ceb73168bfc3d21fb9a8ba90655
dejafait <dejafait@blackhole.com>
2024-01-08 10:40:50 +0100

    Revert "clarify variable names"
    This reverts commit 5f986a2568e488b626493c79f23041ac888df68c.

--- dbt/models/marts/daily/nb_utilisateurs_potentiels.py
+++ dbt/models/marts/daily/nb_utilisateurs_potentiels.py
@@ -8,46 +8,46 @@ def model(dbt, session):
     depts = [d for d in organisations["dept_org"].unique() if d is not None]
     regions = [r for r in organisations["région_org"].unique() if r is not None]
 
-    potential_records = []
+    potentiel_records = []
 
     for dept in depts:
         num_dept = dept.split(" ")[0]
-        for region in regions:
+        for reg in regions:
             # organisations
             for orga_type in organisations["type"].unique():
-                potential = len(
+                cur_pot = len(
                     organisations[
                         (organisations["type"] == orga_type)
                         & (organisations["dept_org"] == dept)
-                        & (organisations["région_org"] == region)
+                        & (organisations["région_org"] == reg)
                     ]
                 )
-                potential_records.append([region, num_dept, dept, "prescripteur", orga_type, potential])
+                potentiel_records.append([reg, num_dept, dept, "prescripteur", orga_type, cur_pot])
 
             # institutions
             for inst_type in institutions["type"].unique():
-                potential = len(
+                cur_pot = len(
                     institutions[
                         (institutions["type"] == inst_type)
                         & (institutions["nom_département"] == dept)
-                        & (institutions["région"] == region)
+                        & (institutions["région"] == reg)
                     ]
                 )
-                potential_records.append([region, num_dept, dept, "institution", inst_type, potential])
+                potentiel_records.append([reg, num_dept, dept, "institution", inst_type, cur_pot])
 
             # structures
             structures = structures[structures["active"] == 1]
             for struct_type in structures["type"].unique():
-                potential = len(
+                cur_pot = len(
                     structures[
                         (structures["type"] == struct_type)
                         & (structures["nom_département"] == dept)
-                        & (structures["région"] == region)
+                        & (structures["région"] == reg)
                     ]
                 )
-                potential_records.append([region, num_dept, dept, "siae", struct_type, potential])
+                potentiel_records.append([reg, num_dept, dept, "siae", struct_type, cur_pot])
 
     return pd.DataFrame.from_records(
-        potential_records,
-        columns=["région", "département_num", "département", "type_utilisateur", "profil", "potential"],
+        potentiel_records,
+        columns=["région", "département_num", "département", "type_utilisateur", "profil", "potentiel"],
     )
