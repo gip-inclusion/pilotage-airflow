90561aa9203df158ba0fb53395f0827c29c6c13a
laurinehu <laurinehu@gmail.com>
2024-01-10 11:09:11 +0100

    add global taux couv for all tbs

---
+++ dbt/models/marts/daily/taux_couverture_global_mensuel.sql
@@ -0,0 +1,24 @@
+select
+    visites.mois,
+    potentiel."région",
+    potentiel."département_num",
+    potentiel.type_utilisateur,
+    potentiel.profil,
+    visites.nb_organisations                                                     as nb_visites,
+    potentiel.potentiel                                                          as potentiel,
+    cast(visites.nb_organisations as float) / cast(potentiel.potentiel as float) as taux_couv
+from {{ ref('nb_utilisateurs_potentiels') }} as potentiel
+left join {{ ref('suivi_visites_tous_tb_prive_mensuel') }} as visites
+    on
+        visites.type_utilisateur = potentiel.type_utilisateur
+        and visites.profil = potentiel.profil
+        and visites."département_num" = potentiel."département_num"
+        and visites."région" = potentiel."région"
+group by
+    visites.mois,
+    potentiel."région",
+    potentiel."département_num",
+    potentiel.type_utilisateur,
+    potentiel.profil,
+    visites.nb_organisations,
+    potentiel.potentiel
