14474aa5b0cb32bc7ef874d78fda25b45afc80f4
laurinehu <laurinehu@gmail.com>
2024-01-10 11:09:11 +0100

    add global taux couv for all tbs

---
+++ dbt/models/marts/daily/suivi_visites_tous_tb_prive_mensuel.sql
@@ -0,0 +1,22 @@
+select
+    visits.region                               as "région",
+    visits.departement                          as "département_num",
+    visits.type_utilisateur                     as type_utilisateur,
+    visits.type_organisation                    as profil,
+    date_trunc('month', visits.semaine)         as mois,
+    -- organisations venues ce trimestre
+    array_agg(distinct visits.nom_organisation) as liste_organisations,
+    count(distinct visits.nom_organisation)     as nb_organisations,
+    -- mail des utilisateurs venus ce trimestre
+    array_agg(distinct c1_users.email)          as liste_utilisateurs,
+    -- nb total d'utilisateurs ce trimestre
+    count(distinct c1_users.email)              as nb_utilisateurs
+from {{ ref('suivi_utilisateurs_tb_prive_semaine') }} as visits
+left join {{ source('emplois', 'utilisateurs') }} as c1_users
+    on c1_users.id = cast(visits.id_utilisateur as integer)
+group by
+    date_trunc('month', visits.semaine),
+    visits.region,
+    visits.departement,
+    visits.type_utilisateur,
+    visits.type_organisation
