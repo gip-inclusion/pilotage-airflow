f1da8ce6ca5218f268a6863ce2367ff789de961e
laurinehu <laurinehu@gmail.com>
2024-01-18 16:47:04 +0100

    change département_num to département move float cast before doing the division move a weekly file that I forgot to move in weekly

--- dbt/models/marts/weekly/suivi_visites_tb_prive_mois.sql
+++ dbt/models/marts/weekly/suivi_visites_tb_prive_mois.sql
@@ -1,17 +1,15 @@
 select
     visits.nom_tb,
-    visits.region                               as "région",
-    visits.departement                          as "département_num",
-    visits.type_utilisateur                     as type_utilisateur,
-    visits.type_organisation                    as profil,
-    date_trunc('month', visits.semaine)         as mois,
+    visits.region                                          as "région",
+    visits.departement                                     as "département_num",
+    visits.type_utilisateur                                as type_utilisateur,
+    visits.type_organisation                               as profil,
+    cast(count(distinct visits.nom_organisation) as float) as nb_organisations,
+    date_trunc('month', visits.semaine)                    as mois,
     -- organisations venues ce mois
-    array_agg(distinct visits.nom_organisation) as liste_organisations,
-    count(distinct visits.nom_organisation)     as nb_organisations,
+    array_agg(distinct visits.nom_organisation)            as liste_organisations,
     -- mail des utilisateurs venus ce mois
-    array_agg(distinct c1_users.email)          as liste_utilisateurs,
-    -- nb total d'utilisateurs cette semaine
-    count(distinct c1_users.email)              as nb_utilisateurs
+    array_agg(distinct c1_users.email)                     as liste_utilisateurs
 from {{ ref('suivi_utilisateurs_tb_prive_semaine') }} as visits
 left join {{ source('emplois', 'utilisateurs') }} as c1_users
     on c1_users.id = cast(visits.id_utilisateur as integer)
