ec4b87e439c0ca033fa16a28aa39f2eeebaaa113
laurinehu <laurinehu@gmail.com>
2024-01-18 16:47:04 +0100

    change département_num to département move float cast before doing the division move a weekly file that I forgot to move in weekly

--- dbt/models/marts/weekly/taux_couverture_global_mensuel.sql
+++ dbt/models/marts/weekly/taux_couverture_global_mensuel.sql
@@ -1,23 +1,23 @@
 select
     visites.mois,
     potentiel."région",
-    potentiel."département_num",
+    potentiel."département",
     potentiel.type_utilisateur,
     potentiel.profil,
-    visites.nb_organisations                                                     as nb_visites,
-    potentiel.potentiel                                                          as potentiel,
-    cast(visites.nb_organisations as float) / cast(potentiel.potentiel as float) as taux_couv
+    visites.nb_organisations                       as nb_visites,
+    potentiel.potentiel                            as potentiel,
+    visites.nb_organisations / potentiel.potentiel as taux_couv
 from {{ ref('nb_utilisateurs_potentiels') }} as potentiel
 left join {{ ref('suivi_visites_tous_tb_prive_mensuel') }} as visites
     on
         visites.type_utilisateur = potentiel.type_utilisateur
         and visites.profil = potentiel.profil
-        and visites."département_num" = potentiel."département_num"
+        and visites."département" = potentiel."département"
         and visites."région" = potentiel."région"
 group by
     visites.mois,
     potentiel."région",
-    potentiel."département_num",
+    potentiel."département",
     potentiel.type_utilisateur,
     potentiel.profil,
     visites.nb_organisations,
