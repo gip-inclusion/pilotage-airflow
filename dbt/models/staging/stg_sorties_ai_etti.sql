9caffaf11db661e1f541f8e6eba5fb75058f4e3a
YannickPassa <yannick.passarelli@gmail.com>
2024-01-23 21:11:48 +0100

    date fix

--- dbt/models/staging/stg_sorties_ai_etti.sql
+++ dbt/models/staging/stg_sorties_ai_etti.sql
@@ -17,4 +17,5 @@ group by
     {{ pilo_star(ref('sorties_v2'), relation_alias="sorties",
     except=["emi_nb_heures_travail", "emi_sme_version", "af_id_annexe_financiere", "emi_afi_id", "emi_sme_annee", "emi_sme_mois"]) }},
     disp.type_structure,
-    disp.type_structure_emplois
+    disp.type_structure_emplois,
+    nbr_h.total_heures_travaillees_derniere_af
