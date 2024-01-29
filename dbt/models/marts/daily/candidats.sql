79035d78925e208f244e8a226620d05adec5a543
laurinehu <laurinehu@gmail.com>
2024-01-29 20:25:12 +0100

    add maj + change non communiqué to non renseigné

--- dbt/models/marts/daily/candidats.sql
+++ dbt/models/marts/daily/candidats.sql
@@ -1,16 +1,16 @@
 select
     {{ pilo_star(ref('stg_candidats')) }},
     case
-        when age_selon_nir <= 25 then 'jeune (- de 26 ans)'
-        when age_selon_nir > 25 and age_selon_nir <= 54 then 'adulte (26-54 ans)'
-        when age_selon_nir >= 55 then 'senior (55 ans et +)'
-        else 'non communiqué'
+        when age_selon_nir <= 25 then 'Jeune (- de 26 ans)'
+        when age_selon_nir > 25 and age_selon_nir <= 54 then 'Adulte (26-54 ans)'
+        when age_selon_nir >= 55 then 'Senior (55 ans et +)'
+        else 'Non renseigné'
     end as tranche_age,
     case
-        when age_selon_nir > 16 and age_selon_nir <= 25 then 'eligible CEJ'
-        when age_selon_nir > 16 and age_selon_nir <= 30 then 'eligible CEJ si RQTH'
-        when age_selon_nir >= 57 then 'eligible CDI inclusion'
-        else 'non communiqué'
+        when age_selon_nir > 16 and age_selon_nir <= 25 then 'Eligible CEJ'
+        when age_selon_nir > 16 and age_selon_nir <= 30 then 'Eligible CEJ si RQTH'
+        when age_selon_nir >= 57 then 'Eligible CDI inclusion'
+        else 'Non renseigné'
     end as eligibilite_dispositif,
     case
         when age_selon_nir > 16 and age_selon_nir <= 25 then 'OUI'
