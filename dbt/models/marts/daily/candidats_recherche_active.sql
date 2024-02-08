12fad4e79138900965220fbdb51fcd82c29dabf7
laurinehu <laurinehu@gmail.com>
2024-02-08 18:26:57 +0100

    add siae filter

--- dbt/models/marts/daily/candidats_recherche_active.sql
+++ dbt/models/marts/daily/candidats_recherche_active.sql
@@ -5,13 +5,14 @@ select
     type_auteur_diagnostic,
     "région",
     "nom_département",
-    max(date_candidature)                                                                   as date_derniere_candidature,
-    current_date - max(date_candidature)                                                    as delai_derniere_candidature,
-    current_date - max(case when "état" = 'Candidature acceptée' then date_candidature end) as delai_derniere_candidature_acceptee,
-    max(case when "état" = 'Candidature acceptée' then date_candidature end)                as date_derniere_candidature_acceptee,
-    sum(case when "état" = 'Candidature acceptée' then 1 else 0 end)                        as nb_candidatures_acceptees,
-    sum(case when "état" != 'Candidature acceptée' then 1 else 0 end)                       as nb_candidatures_sans_accept,
-    coalesce(sum(case when "état" = 'Candidature acceptée' then 1 else 0 end) > 0)          as a_eu_acceptation
+    coalesce(sum(case when "type_structure" in ('ACI', 'AI', 'EI', 'EITI', 'ETTI') then 1 else 0 end) > 0) as candidat_iae,
+    max(date_candidature)                                                                                  as date_derniere_candidature,
+    current_date - max(date_candidature)                                                                   as delai_derniere_candidature,
+    current_date - max(case when "état" = 'Candidature acceptée' then date_candidature end)                as delai_derniere_candidature_acceptee,
+    max(case when "état" = 'Candidature acceptée' then date_candidature end)                               as date_derniere_candidature_acceptee,
+    sum(case when "état" = 'Candidature acceptée' then 1 else 0 end)                                       as nb_candidatures_acceptees,
+    sum(case when "état" != 'Candidature acceptée' then 1 else 0 end)                                      as nb_candidatures_sans_accept,
+    coalesce(sum(case when "état" = 'Candidature acceptée' then 1 else 0 end) > 0)                         as a_eu_acceptation
 from {{ ref('stg_candidats_candidatures') }}
 where date_candidature >= current_date - interval '6 months'
 group by
