f71037d1059576d2bb23165b11cf17b86ed18acb
laurinehu <laurinehu@gmail.com>
2024-03-08 12:06:03 +0100

    get ville data from insee cog

--- dbt/models/marts/daily/structures.sql
+++ dbt/models/marts/daily/structures.sql
@@ -1,13 +1,17 @@
 select
-    {{ pilo_star(source('emplois','structures_v0'), except=['source'], relation_alias='s') }},
-    grp_strct.groupe as categorie_structure,
+    {{ pilo_star(source('emplois','structures_v0'), except=['source', 'ville'], relation_alias='struct') }},
+    insee.libelle_commune as ville,
+    grp_strct.groupe      as categorie_structure,
     case
-        when type = 'OPCS' and source = 'Utilisateur (Antenne)' then 'Utilisateur (OPCS)'
-        when type = 'OPCS' and source = 'Staff Itou' then 'Staff Itou (OPCS)'
-        else source
-    end              as source
+        when struct.type = 'OPCS' and struct.source = 'Utilisateur (Antenne)' then 'Utilisateur (OPCS)'
+        when struct.type = 'OPCS' and struct.source = 'Staff Itou' then 'Staff Itou (OPCS)'
+        else struct.source
+    end                   as source
 from
-    {{ source('emplois','structures_v0') }} as s
+    {{ source('emplois','structures_v0') }} as struct
 left join
     {{ ref('groupes_structures') }} as grp_strct
-    on grp_strct.structure = s.type
+    on grp_strct.structure = struct.type
+left join
+    {{ ref('insee_communes') }} as insee
+    on coalesce(ltrim(struct.code_commune, '0'), ltrim(struct.code_commune_c1, '0')) = insee.code_insee
