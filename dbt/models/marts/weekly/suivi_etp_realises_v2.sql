0ca0c558e45324ab90c1278fe41c95586bf0c3c0
YannickPassa <yannick.passarelli@gmail.com>
2024-04-11 15:06:12 +0200

    structure id
    group

--- dbt/models/marts/weekly/suivi_etp_realises_v2.sql
+++ dbt/models/marts/weekly/suivi_etp_realises_v2.sql
@@ -20,6 +20,7 @@ select distinct
     af.af_mesure_dispositif_code,
     ref_asp.type_structure,
     ref_asp.type_structure_emplois,
+    structure.structure_id_siae,
     structure.structure_denomination,
     structure.structure_adresse_admin_commune                                           as commune_structure,
     structure.structure_adresse_admin_code_insee                                        as code_insee_structure,
