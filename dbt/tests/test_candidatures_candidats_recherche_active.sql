d6df416ccd9574fc6d3d1f50263f9b4c42c62759
YannickPassa <yannick.passarelli@gmail.com>
2024-04-25 08:08:54 +0200

    test

---
+++ dbt/tests/test_candidatures_candidats_recherche_active.sql
@@ -0,0 +1,17 @@
+with nb_candidatures as (
+    select
+        (
+            select count(*)
+            from candidatures_echelle_locale
+            where date_candidature >= current_date - interval '6 months'
+        ) as candidatures_cel,
+        (
+            select count(*)
+            from candidatures_candidats_recherche_active
+            where date_candidature >= current_date - interval '6 months'
+        ) as candidatures_cra
+)
+
+select *
+from nb_candidatures
+where candidatures_cel != candidatures_cra
