51eff6aaf5820889667c0b4a085964e1a4d44d18
laurinehu <laurinehu@gmail.com>
2024-04-25 11:20:20 +0200

    add total nb of candidatures

--- dbt/models/marts/daily/candidatures_candidats_recherche_active.sql
+++ dbt/models/marts/daily/candidatures_candidats_recherche_active.sql
@@ -1,3 +1,4 @@
-select {{ pilo_star(ref('stg_candidats_candidatures')) }}
-from {{ ref('stg_candidats_candidatures') }}
-where date_candidature >= current_date - interval '6 months'
+select {{ pilo_star(ref('stg_candidats_candidatures'), relation_alias="cdd") }}
+from {{ ref('stg_candidats_candidatures') }} as cdd
+right join {{ ref('candidats_recherche_active') }} as cra
+    on cra.id = cdd.id
