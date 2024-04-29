7b6981785e05c35835daaf7099063da83e5222eb
laurinehu <laurinehu@gmail.com>
2024-04-29 16:16:22 +0200

    remove applications that have more than 6 months

--- dbt/models/marts/daily/candidatures_candidats_recherche_active.sql
+++ dbt/models/marts/daily/candidatures_candidats_recherche_active.sql
@@ -2,3 +2,5 @@ select {{ pilo_star(ref('stg_candidats_candidatures'), relation_alias="cdd") }}
 from {{ ref('stg_candidats_candidatures') }} as cdd
 right join {{ ref('candidats_recherche_active') }} as cra
     on cra.id = cdd.id
+-- certains candidats en recherche active ont reçu des candidatures avant 6 mois et elles ne nous intéressent pas ici
+where cdd.date_candidature >= current_date - interval '6 months'
