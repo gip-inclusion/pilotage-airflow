60d5b18532f0dcb6e832a6c075ac234a1814b5c9
laurinehu <laurinehu@gmail.com>
2024-05-05 12:26:39 +0200

    fix contrats

--- dbt/models/staging/stg_contrats.sql
+++ dbt/models/staging/stg_contrats.sql
@@ -1,9 +1,13 @@
 select distinct
     ctr.contrat_id_pph,
     ctr.contrat_id_ctr,
+    ctr.contrat_id_structure,
+    ctr.contrat_mesure_disp_code,
+    ctr.contrat_duree_contrat,
     ctr.contrat_date_embauche,
     ctr.contrat_date_fin_contrat,
     ctr.contrat_type_contrat,
+    sum(emi.emi_nb_heures_travail)                                                 as contrat_nb_heures,
     to_date(ctr.contrat_date_sortie_definitive, 'DD/MM/YYYY')                      as contrat_date_sortie_definitive,
     sum(case
         when ctr.contrat_type_contrat = 0 then 1
@@ -12,3 +16,13 @@ select distinct
 from {{ ref('fluxIAE_EtatMensuelIndiv_v2') }} as emi
 left join {{ ref('fluxIAE_ContratMission_v2') }} as ctr
     on ctr.contrat_id_ctr = emi.emi_ctr_id
+group by
+    ctr.contrat_id_pph,
+    ctr.contrat_id_ctr,
+    ctr.contrat_id_structure,
+    ctr.contrat_mesure_disp_code,
+    ctr.contrat_duree_contrat,
+    ctr.contrat_date_embauche,
+    ctr.contrat_date_fin_contrat,
+    ctr.contrat_type_contrat,
+    ctr.contrat_date_sortie_definitive
