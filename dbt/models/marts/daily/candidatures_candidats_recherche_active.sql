13a061a54ee96cd264aaa7536efd66931c910b0c
laurinehu <laurinehu@gmail.com>
2024-05-21 15:37:20 +0200

    add date last emitedapplication + nb of accepted applications

--- dbt/models/marts/daily/candidatures_candidats_recherche_active.sql
+++ dbt/models/marts/daily/candidatures_candidats_recherche_active.sql
@@ -1,4 +1,7 @@
-select {{ pilo_star(ref('stg_candidats_candidatures'), relation_alias="cdd") }}
+select
+    {{ pilo_star(ref('stg_candidats_candidatures'), relation_alias="cdd") }},
+    cra.nb_candidatures_acceptees,
+    cra.delai_derniere_candidature
 from {{ ref('stg_candidats_candidatures') }} as cdd
 right join {{ ref('candidats_recherche_active') }} as cra
     on cra.id = cdd.id
