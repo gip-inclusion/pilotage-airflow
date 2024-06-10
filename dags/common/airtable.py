62bcece89142e03d81178420a778e54bfcb70098
YannickPassa <yannick.passarelli@gmail.com>
2024-06-10 15:53:37 +0200

    getting ALL the rows
    comment
    
    comment for laulau

--- dags/common/airtable.py
+++ dags/common/airtable.py
@@ -14,10 +14,17 @@ def connection_airtable(table_name):
 def fetch_airtable_data(url, headers):
     records = []
     params = {}
-    response = requests.get(url=url, params=params, headers=headers)
-    if response.status_code != 200:
-        raise Exception(f"Failed to fetch data: {response.status_code}, {response.text}")
-    data = response.json()
-    records.extend(data["records"])
+    while True:
+        response = requests.get(url=url, params=params, headers=headers)
+        if response.status_code != 200:
+            raise Exception(f"Failed to fetch data: {response.status_code}, {response.text}")
+        data = response.json()
+        records.extend(data["records"])
+        # Airtable is limited to 100 rows per request
+        # the if combined with 'while true' will add rows until there are no more rows to loop in
+        if "offset" in data:
+            params["offset"] = data["offset"]
+        else:
+            break
     df = pd.DataFrame([record["fields"] for record in records])
     return df
