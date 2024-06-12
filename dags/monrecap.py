6edaec0f5eaf16bd534880b679bf59527b1ae0d4
YannickPassa <yannick.passarelli@gmail.com>
2024-06-12 15:50:26 +0200

    datetime
    fix
    
    dates change

--- dags/monrecap.py
+++ dags/monrecap.py
@@ -17,11 +17,23 @@ with DAG(
 
     @task(task_id="monrecap_airtable")
     def monrecap_airtable(**kwargs):
+        import pandas as pd
+
         tables = ["Commandes", "Contacts"]
         for table_name in tables:
             url, headers = airtable.connection_airtable(table_name)
             df = airtable.fetch_airtable_data(url, headers)
-            df.to_sql(table_name, con=db_monrecap.connection_engine_monrecap(), if_exists="replace", index=False)
+            if table_name == "Commandes":
+                df["Submitted at"] = pd.to_datetime(df["Submitted at"])
+            if table_name == "Contacts":
+                df["Date de première commande"] = pd.to_datetime(df["Date de première commande"])
+                df["Date de dernière commande"] = pd.to_datetime(df["Date de dernière commande"])
+            df.to_sql(
+                table_name,
+                con=db_monrecap.connection_engine_monrecap(),
+                if_exists="replace",
+                index=False,
+            )
 
     monrecap_airtable = monrecap_airtable()
 
@@ -32,6 +44,7 @@ with DAG(
         sheet_url = Variable.get("BAROMETRE_MON_RECAP")
         print(f"reading barometre monrecap at {sheet_url=}")
         df_baro = pd.read_csv(sheet_url)
+        df_baro["Submitted at"] = pd.to_datetime(df_baro["Submitted at"])
         df_baro.to_sql("barometre", con=db_monrecap.connection_engine_monrecap(), if_exists="replace", index=False)
 
     monrecap_gsheet = monrecap_gsheet()
