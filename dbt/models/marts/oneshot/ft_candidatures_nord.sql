f4d98e52b3fe211903248e2dbcba8f9808601996
laurinehu <laurinehu@gmail.com>
2024-07-22 11:49:03 +0200

    dbt: patch to correct dbt_daily

--- dbt/models/marts/oneshot/ft_candidatures_nord.sql
+++
@@ -1,8 +0,0 @@
-select
-    {{ pilo_star(source('oneshot', 'ft_iae_nord'), relation_alias='ft') }},
-    {{ pilo_star(ref('candidatures_echelle_locale'), relation_alias='cd') }}
-from {{ source('oneshot', 'ft_iae_nord') }} as ft
-left join {{ ref('candidats') }} as c
-    on ft.nir_hash = c.hash_nir
-left join {{ ref('candidatures_echelle_locale') }} as cd
-    on c.id = cd.id_candidat
--- dbt/models/staging/stg_candidats_asp_derniere_sortie.sql
+++ dbt/models/staging/stg_candidats_asp_derniere_sortie.sql
@@ -1,11 +1,11 @@
 select
-    pass."hash_numéro_pass_iae"                      as pass_iae,
-    c.id                                             as id_salarie_emplois,
-    salarie.salarie_id                               as id_salarie_asp,
-    sortie.rcs_libelle                               as cause_sortie,
-    sortie.rms_libelle                               as motif_sortie,
-    max(sortie.contrat_date_sortie_definitive::date) as date_derniere_sortie,
-    max(cddr.date_candidature)                       as date_derniere_candidature
+    pass."hash_numéro_pass_iae"                                       as pass_iae,
+    c.id                                                              as id_salarie_emplois,
+    salarie.salarie_id                                                as id_salarie_asp,
+    sortie.rcs_libelle                                                as cause_sortie,
+    sortie.rms_libelle                                                as motif_sortie,
+    to_date(max(sortie.contrat_date_sortie_definitive), 'DD/MM/YYYY') as date_derniere_sortie,
+    max(cddr.date_candidature)                                        as date_derniere_candidature
 from {{ source('fluxIAE','fluxIAE_Salarie') }} as salarie
 -- pour récupérer le nir pour ensuite pouvoir filtrer les candidats non ft
 left join {{ ref('pass_agrements_valides') }} as pass
