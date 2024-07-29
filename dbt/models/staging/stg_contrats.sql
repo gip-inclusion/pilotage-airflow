f059d0b496b2ec607a7ee4d4f2385778d5683b9d
laurinehu <laurinehu@gmail.com>
2024-07-29 15:24:36 +0200

    calculate aci contracts

--- dbt/models/staging/stg_contrats.sql
+++ dbt/models/staging/stg_contrats.sql
@@ -7,6 +7,7 @@ select distinct
     ctr.contrat_date_embauche,
     ctr.contrat_date_fin_contrat,
     ctr.contrat_type_contrat,
+    motif_sortie.rms_libelle                                                       as motif_sortie,
     sum(emi.emi_nb_heures_travail)                                                 as contrat_nb_heures,
     to_date(ctr.contrat_date_sortie_definitive, 'DD/MM/YYYY')                      as contrat_date_sortie_definitive,
     sum(case
@@ -16,6 +17,8 @@ select distinct
 from {{ ref('fluxIAE_EtatMensuelIndiv_v2') }} as emi
 left join {{ ref('fluxIAE_ContratMission_v2') }} as ctr
     on ctr.contrat_id_ctr = emi.emi_ctr_id
+left join {{ ref('fluxIAE_RefMotifSort_v2') }} as motif_sortie
+    on emi.emi_motif_sortie_id = motif_sortie.rms_id
 group by
     ctr.contrat_id_pph,
     ctr.contrat_id_ctr,
@@ -25,4 +28,7 @@ group by
     ctr.contrat_date_embauche,
     ctr.contrat_date_fin_contrat,
     ctr.contrat_type_contrat,
-    ctr.contrat_date_sortie_definitive
+    ctr.contrat_date_sortie_definitive,
+    motif_sortie.rms_libelle
+-- because we consider only contracts starting in 2021
+having extract(year from ctr.contrat_date_embauche) >= 2021
