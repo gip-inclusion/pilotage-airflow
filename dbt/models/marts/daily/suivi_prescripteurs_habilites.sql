0c7536879ca183c4e07c31a2eba0fead895d170d
Romain Sébille <romain.sebille@beta.gouv.fr>
2024-07-31 10:05:07 +0200

    dbt: Handle all possible values for the 'orienteur' fake organization

--- dbt/models/marts/daily/suivi_prescripteurs_habilites.sql
+++ dbt/models/marts/daily/suivi_prescripteurs_habilites.sql
@@ -7,4 +7,4 @@ select
 from {{ ref('stg_organisations') }} as organisations
 left join {{ ref("stg_siae_partenaires_prescripteurs") }} as nb_siae_partenaires
     on organisations.id = nb_siae_partenaires.id
-where organisations.type != 'SANS-ORGANISATION' and organisations."habilitée" = 1
+where organisations.type != 'SANS-ORGANISATION' and organisations.type is not null and organisations."habilitée" = 1
