97f06493633c95bb310e1b787eab73e1eda112ab
Yannick Passarelli <yannick.passarelli@gmail.com>
2024-08-01 10:30:54 +0200

    removing the oneshot from the daily dbt

--- dags/dbt_daily.py
+++ dags/dbt_daily.py
@@ -39,7 +39,7 @@ with airflow.DAG(
         is_full_refresh = params.get("full_refresh")
         if is_full_refresh:
             kwargs["ti"].xcom_push("dbt_seed_args", "--full-refresh")
-            kwargs["ti"].xcom_push("dbt_run_args", "--full-refresh --exclude marts.weekly marts.manual")
+            kwargs["ti"].xcom_push("dbt_run_args", "--full-refresh --exclude marts.weekly marts.manual marts.oneshot")
         else:
             kwargs["ti"].xcom_push("dbt_seed_args", "")
             kwargs["ti"].xcom_push("dbt_run_args", "--select marts.daily legacy.daily ephemeral staging")
