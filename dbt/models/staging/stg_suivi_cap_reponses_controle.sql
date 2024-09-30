4b75a17f3587bbcdd0b26c8cd780e8ee969c4ed2
Romain Sébille <romain.sebille@beta.gouv.fr>
2024-09-30 11:47:40 +0200

    dbt: Fix `rule-structure.subquery` rule violation

--- dbt/models/staging/stg_suivi_cap_reponses_controle.sql
+++ dbt/models/staging/stg_suivi_cap_reponses_controle.sql
@@ -1,14 +1,14 @@
--- todo (laurine) simplifier la requête imbriquée redondante (dbt template ?)
-select
-    {{ pilo_star(source('emplois', 'cap_structures')) }},
-    'NON' as "réponse_au_contrôle"
-from cap_structures
-left join (
+with subquery as (
     select distinct cap_candidatures.id_cap_structure
     from "cap_critères_iae"
     inner join cap_candidatures
         on "cap_critères_iae".id_cap_candidature = cap_candidatures.id
-) as subquery
+)
+select
+    {{ pilo_star(source('emplois', 'cap_structures')) }},
+    'NON' as "réponse_au_contrôle"
+from cap_structures
+left join subquery
     on cap_structures.id = subquery.id_cap_structure
 where subquery.id_cap_structure is null
 
@@ -18,11 +18,6 @@ select
     {{ pilo_star(source('emplois', 'cap_structures')) }},
     'OUI' as "réponse_au_contrôle"
 from cap_structures
-left join (
-    select distinct cap_candidatures.id_cap_structure
-    from "cap_critères_iae"
-    inner join cap_candidatures
-        on "cap_critères_iae".id_cap_candidature = cap_candidatures.id
-) as subquery
+left join subquery
     on cap_structures.id = subquery.id_cap_structure
 where subquery.id_cap_structure is not null
