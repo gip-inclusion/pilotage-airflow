e1f22b7b417f2aca2cd48e48a7561314dd49b26c
laurinehu <laurinehu@gmail.com>
2024-10-08 16:43:47 +0200

    resolve conflict

--- dbt/models/indexed/fluxIAE_Structure_v2.sql
+++ dbt/models/indexed/fluxIAE_Structure_v2.sql
@@ -8,10 +8,11 @@
 select
     {{ pilo_star(source('fluxIAE', 'fluxIAE_Structure')) }},
     -- l'ASP préconise l'utilisation de l'adresse administrative pour récupérer la commune de la structure
-    app_geo.nom_epci                as nom_epci_structure,
-    app_geo.nom_region              as nom_region_structure,
-    app_geo.code_dept               as code_dept_structure,
-    app_geo.nom_departement_complet as nom_departement_structure
+    app_geo.nom_epci                                                  as nom_epci_structure,
+    app_geo.nom_region                                                as nom_region_structure,
+    app_geo.code_dept                                                 as code_dept_structure,
+    app_geo.nom_departement_complet                                   as nom_departement_structure,
+    concat_ws('-', structure_denomination, structure_siret_actualise) as structure_denomination_unique
 from
     {{ source('fluxIAE', 'fluxIAE_Structure') }} as structure
 left join {{ ref('stg_insee_appartenance_geo_communes') }} as app_geo
