0f06e67b10e6924775a0444049f7d0c29c8769d3
JulesLXI <jules.lefebvre@beta.gouv.fr>
2024-10-24 15:31:51 +0200

    Dag pour refresh les oneshot
    Dag for oneshots

---
+++ dags/dbt_oneshot.py
@@ -0,0 +1,59 @@
+import airflow
+from airflow.operators import bash, empty, trigger_dagrun
+
+from dags.common import db, dbt, default_dag_args, slack
+
+
+dag_args = default_dag_args() | {"default_args": dbt.get_default_args()}
+
+with airflow.DAG(
+    dag_id="dbt_oneshot",
+    schedule_interval=None,
+    **dag_args,
+) as dag:
+    start = empty.EmptyOperator(task_id="start")
+
+    end = slack.success_notifying_task()
+
+    env_vars = db.connection_envvars()
+
+    dbt_debug = bash.BashOperator(
+        task_id="dbt_debug",
+        bash_command="dbt debug",
+        env=env_vars,
+        append_env=True,
+    )
+
+    dbt_deps = bash.BashOperator(
+        task_id="dbt_deps",
+        bash_command="dbt deps",
+        env=env_vars,
+        append_env=True,
+    )
+
+    dbt_seed = bash.BashOperator(
+        task_id="dbt_seed",
+        bash_command="dbt seed --full-refresh",
+        env=env_vars,
+        append_env=True,
+    )
+
+    dbt_run = bash.BashOperator(
+        task_id="dbt_run",
+        bash_command="dbt run --select marts.oneshot",
+        env=env_vars,
+        append_env=True,
+    )
+
+    dbt_clean = bash.BashOperator(
+        task_id="dbt_clean",
+        bash_command="dbt clean",
+        env=env_vars,
+        append_env=True,
+    )
+
+    dag_data_consistency = trigger_dagrun.TriggerDagRunOperator(
+        trigger_dag_id="data_consistency", task_id="trigger_data_consistency"
+    )
+
+    (start >> dbt_debug >> dbt_deps >> dbt_seed >> dbt_run >> dbt_clean >> dag_data_consistency >> end)
