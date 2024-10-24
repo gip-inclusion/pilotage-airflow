52095e92569987061e5548a8080890e27a2c83cc
JulesLXI <jules.lefebvre@beta.gouv.fr>
2024-10-24 15:31:51 +0200

    Dag pour refresh les oneshot

---
+++ dags/dbt_oneshot.py
@@ -0,0 +1,72 @@
+import airflow
+from airflow.models.param import Param
+from airflow.operators import bash, empty, python, trigger_dagrun
+
+from dags.common import db, dbt, default_dag_args, slack
+
+
+dag_args = default_dag_args() | {"default_args": dbt.get_default_args()}
+
+with airflow.DAG(
+    dag_id="dbt_oneshot",
+    schedule_interval=None,
+    params={"refresh_oneshot": Param(False, type="boolean")},
+    **dag_args,
+) as dag:
+    start = empty.EmptyOperator(task_id="start")
+
+    end = slack.success_notifying_task()
+
+    env_vars = db.connection_envvars()
+
+    dbt_debug = bash.BashOperator(
+        task_id="dbt_debug",
+        bash_command="dbt debug",
+        env=env_vars,
+        append_env=True,
+    )
+
+    dbt_deps = bash.BashOperator(
+        task_id="dbt_deps",
+        bash_command="dbt deps",
+        env=env_vars,
+        append_env=True,
+    )
+
+    def params_check(params=None, **kwargs):
+        is_refresh_oneshot = params.get("refresh_oneshot")
+        if is_refresh_oneshot:
+            kwargs["ti"].xcom_push("dbt_seed_args", "--full-refresh")
+            kwargs["ti"].xcom_push("dbt_run_args", "--select marts.oneshot")
+        else:
+            kwargs["ti"].xcom_push("dbt_seed_args", "")
+            kwargs["ti"].xcom_push("dbt_run_args", "--select candidatures_ddets50")
+
+    params_check = python.PythonOperator(task_id="params_check", provide_context=True, python_callable=params_check)
+
+    dbt_seed = bash.BashOperator(
+        task_id="dbt_seed",
+        bash_command="dbt seed {{ ti.xcom_pull(task_ids='params_check', key='dbt_seed_args') }}",
+        env=env_vars,
+        append_env=True,
+    )
+
+    dbt_run = bash.BashOperator(
+        task_id="dbt_run",
+        bash_command="dbt run {{ ti.xcom_pull(task_ids='params_check', key='dbt_run_args') }}",
+        env=env_vars,
+        append_env=True,
+    )
+
+    dbt_clean = bash.BashOperator(
+        task_id="dbt_clean",
+        bash_command="dbt clean",
+        env=env_vars,
+        append_env=True,
+    )
+
+    dag_data_consistency = trigger_dagrun.TriggerDagRunOperator(
+        trigger_dag_id="data_consistency", task_id="trigger_data_consistency"
+    )
+
+    (start >> dbt_debug >> dbt_deps >> dbt_seed >> dbt_run >> dbt_clean >> dag_data_consistency >> end)
