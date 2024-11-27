6b1272d687dd809d935dff848dec378b359167ee
Yannick Passarelli <yannick.passarelli@gmail.com>
2024-11-27 10:28:37 +0100

    addition of the prescriber arrondissement

--- dbt/models/marts/daily/suivi_candidatures_prescripteurs_habilites.sql
+++ dbt/models/marts/daily/suivi_candidatures_prescripteurs_habilites.sql
@@ -1,11 +1,12 @@
 select
     {{ pilo_star(ref('candidatures_echelle_locale'), relation_alias='candidatures') }},
-    organisations."département"     as "département_prescripteur",
-    organisations."nom_département" as "nom_département_prescripteur",
-    organisations."région"          as "région_prescripteur",
-    organisations.epci              as epci_prescripteur,
-    organisations.zone_emploi       as zone_emploi_prescripteur,
-    organisations.type_complet      as libelle_complet
+    organisations."département"      as "département_prescripteur",
+    organisations."nom_département"  as "nom_département_prescripteur",
+    organisations."région"           as "région_prescripteur",
+    organisations.nom_arrondissement as nom_arrondissement_prescripteur,
+    organisations.epci               as epci_prescripteur,
+    organisations.zone_emploi        as zone_emploi_prescripteur,
+    organisations.type_complet       as libelle_complet
 from
     {{ ref('candidatures_echelle_locale') }} as candidatures
 left join {{ ref('organisations') }} as organisations
