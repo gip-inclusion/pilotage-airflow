5f1335da4fa265da0ce9a93e75986e11765fddeb
Kobus <sonia.perelroizen@inclusion.gouv.fr>
2024-12-10 11:29:39 +0100

    indicators metabase hiring

---
+++ scripts/metabase/497_recrutements_siae.sql
@@ -0,0 +1,21 @@
+--https://stats.inclusion.beta.gouv.fr/question/5428
+Select
+type_siae as SIAE,
+count(distinct recrutements.identifiant_contrat) as recrutements
+From recrutements
+where type_siae not in ('ACIPA','EIPA')
+and {{annee_recrutement}}
+and {{genre}}
+and {{qpv}}
+and {{zrr}}
+and {{brsa}}
+and {{rqth}}
+and {{niveau_formation_salarie}}
+and {{structure_denomination}}
+and {{nom_departement_af}}
+and {{nom_region_af}}
+and {{type_siae}}
+and {{tranche_age}}
+and {{af_numero_annexe_financiere}}
+group by SIAE
+order by recrutements desc
