1676330f12aeb99e69618addce4effdfa4577188
Yannick Passarelli <yannick.passarelli@gmail.com>
2024-12-17 21:06:34 +0100

    changing refs following source + dpt changes
    removing a bad dptmt view

--- dbt/models/monrecap/staging/stg_baro_commandes_starmetric.sql
+++ dbt/models/monrecap/staging/stg_baro_commandes_starmetric.sql
@@ -1,5 +1,5 @@
 select
-    {{ pilo_star(ref('stg_commandes_starmetric'),relation_alias="cmd") }},
+    {{ pilo_star(ref('stg_commandes_starmetric'), relation_alias="cmd") }},
     {{ pilo_star(ref('stg_barometre_starmetric'), relation_alias="baro") }},
     contacts."EMAIL"                                       as email,
     contacts."STRUCTURE"                                   as structure,
@@ -9,5 +9,5 @@ select
 from {{ ref('stg_commandes_starmetric') }} as cmd
 left join {{ ref('stg_barometre_starmetric') }} as baro
     on cmd.email_commande = baro.votre_adresse_mail
-left join {{ source('monrecap', 'Contacts') }} as contacts
+left join {{ ref('Contacts') }} as contacts
     on cmd.email_commande = contacts."EMAIL"
