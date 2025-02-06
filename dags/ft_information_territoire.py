78429ba41d35694c1dc8e5caa66d202f6850b713
Calum Mackervoy <calummackervoy@protonmail.com>
2025-02-06 18:06:25 +0100

    WIP code for FT API DAG

---
+++ dags/ft_information_territoire.py
@@ -0,0 +1,214 @@
+import logging
+from collections import namedtuple
+
+import pandas as pd
+import requests
+from airflow import DAG
+from airflow.decorators import task
+from airflow.models import Variable
+from airflow.operators import python
+
+from dags.common import db, default_dag_args, slack
+from dags.common.errors import ImproperlyConfiguredException
+
+
+DB_SCHEMA = "france_travail"
+REGISTERED_JOBSEEKER_STATS_BY_TERRITORY_ENDPOINT = (
+    "https://api.francetravail.io/partenaire/stats-offres-demandes-emploi/v1/indicateur/stat-demandeurs"
+)
+
+
+Territory = namedtuple("Territory", ["type", "code"])
+
+
+logger = logging.getLogger(__name__)
+
+
+with DAG(
+    "ft_information_territoire",
+    schedule_interval=None,
+    **default_dag_args(),
+) as dag:
+
+    @task(task_id="information_territoire_access_token")
+    def information_territoire_access_token(**kwargs):
+        # Verify credentials.
+        client_id = Variable.get("FT_API_CLIENT_ID")
+        client_secret = Variable.get("FT_API_CLIENT_SECRET")
+
+        if client_id == "" or client_secret == "":
+            raise ImproperlyConfiguredException(
+                "Variables FT_API_CLIENT_ID and FT_API_CLIENT_SECRET must be configured"
+            )
+
+        # Request access to the API using our credentials and required scope.
+        response = requests.post(
+            url="https://entreprise.francetravail.fr/connexion/oauth2/access_token",
+            params={"realm": "/partenaire"},
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+            data={
+                "grant_type": "client_credentials",
+                "client_id": client_id,
+                "client_secret": client_secret,
+                "scope": "api_stats-offres-demandes-emploiv1 offresetdemandesemploi",
+            },
+        )
+        data = response.json()
+
+        # Return token under the form: Bearer xyz
+        return f"{data['token_type']} {data['access_token']}"
+
+    @task(task_id="registered_jobseeker_stats_by_territory")
+    def registered_jobseeker_stats_by_territory(access_token, **kwargs):
+        table_descriptions = {}  # Maps table name to description during import
+
+        def list_territories(access_token, **kwargs):
+            """
+            Fetch a list of regions to use with the API.
+            Preferred to hardcoding the regions/departments because some regions are unavailable.
+            """
+
+            def get_territories_for_type(territory_type):
+                FT_API_ENDPOINT = (
+                    "https://api.francetravail.io/partenaire/stats-offres-demandes-emploi/v1/referentiel/territoires"
+                )
+                response = requests.get(
+                    url=f"{FT_API_ENDPOINT}/{territory_type}",
+                    headers={"Accept": "application/json", "Authorization": access_token},
+                )
+                return [
+                    Territory(type=t["codeTypeTerritoire"], code=t["codeTerritoire"])
+                    for t in response.json()["territoires"]
+                ]
+
+            return get_territories_for_type("REG") + get_territories_for_type("DEP")
+
+        def get_stats_for_territory(territory_type, territory_code):
+            response = requests.post(
+                url=REGISTERED_JOBSEEKER_STATS_BY_TERRITORY_ENDPOINT,
+                headers={
+                    "Accept": "application/json",
+                    "Authorization": access_token,
+                    "Content-Type": "application/json",
+                },
+                json={
+                    "codeTypeTerritoire": territory_type,  # REG / DEP
+                    "codeTerritoire": territory_code,
+                    "codeTypeActivite": "CUMUL",  # CUMUL = All activities
+                    "codeActivite": "CUMUL",
+                    "codeTypePeriode": "TRIMESTRE",
+                    "codeTypeNomenclature": "CATCAND",  # Stats sur le nombre des demandeurs d'emplois
+                },
+            )
+            # NOTE: the response JSON is a very large dictionary
+            response_data = response.json()
+            data_category = response_data["codeFamille"]
+            data = response_data["listeValeursParPeriode"] if "listeValeursParPeriode" in response_data else []
+
+            # The response groups characteristic values by nomenclature,
+            # e.g. by category of jobseeker.
+            # We restructure the data into tables.
+            rows_created = 0
+            connection = db.connection_engine()
+
+            for table in data:
+                # TODO(calum): for protecting against an obscure hypothetical SQL injection,
+                # and more likely against backwards-incompatible table renames,
+                # pull the table_name from a configuration rather than from the API response directly
+                table_name = f"{data_category}_{table['codeNomenclature']}".lower()
+                table_description = table["libNomenclature"]
+                table_descriptions[table_name] = table_description
+
+                # TODO(calum): use keys for these values instead of literals and store them in new tables?
+                data_for_rows = {
+                    key: table[key]
+                    for key in [
+                        "codeTypeTerritoire",
+                        "codeTerritoire",
+                        "libTerritoire",
+                        "codeTypeActivite",
+                        "codeActivite",
+                        "libActivite",
+                        "codeNomenclature",
+                        "libNomenclature",
+                        "codeTypePeriode",
+                        "codePeriode",
+                        "libPeriode",
+                    ]
+                    if key in table
+                }
+                data_for_rows["dateMaj"] = pd.to_datetime(table["datMaj"])
+
+                rows = [row | data_for_rows for row in table["listeValeurParCaract"]]
+
+                # The total isn't included in the list of characteristics, so we add it.
+                rows.append(
+                    {
+                        "codeTypeCaract": "CUMUL",  # Our own value, mimicking their style.
+                        "codeCaract": "CUMUL",
+                        "libCaract": None,
+                        "nombre": table["valeurPrincipaleNombre"],
+                        "pourcentage": table["valeurSecondairePourcentage"],
+                        "masque": False,
+                    }
+                    | data_for_rows
+                )
+
+                df = pd.DataFrame(rows)
+                # Set the index as a unique identifier for the information represented by the row.
+                df.set_index(
+                    [
+                        "codeTypeTerritoire",
+                        "codeTerritoire",
+                        "codeTypeActivite",
+                        "codeActivite",
+                        "codeNomenclature",
+                        "codeTypePeriode",
+                        "codePeriode",
+                    ],
+                    inplace=True,
+                )
+                # TODO: upsert behaviour on this query, if the dateMaj is newer
+                rows_created += df.to_sql(
+                    table_name,
+                    con=connection,
+                    schema=DB_SCHEMA,
+                    if_exists="append",
+                    index=True,
+                )
+                logger.info("Imported %r rows into table %s", rows_created, table_name)
+
+            logger.info(
+                "Import complete for territory %s (%s). Created %r rows across %r tables",
+                territory_code,
+                territory_type,
+                rows_created,
+                len(table_descriptions),
+            )
+
+        # Import data from the API for each territory targeted.
+        for territory in list_territories(access_token):
+            get_stats_for_territory(territory.type, territory.code)
+
+        with db.MetabaseDBCursor() as (cursor, conn):
+            for table_name, table_description in table_descriptions.items():
+                logger.debug("Adding description to %s: %s", table_name, table_description)
+                # TODO: proper cleaning of string
+                table_description = table_description.replace("'", "’")
+                stmt = f"COMMENT ON TABLE {DB_SCHEMA}.{table_name} IS '{table_description}'"
+                cursor.execute(stmt)
+            conn.commit()
+            logger.info("Updated descriptions for all tables")
+        logger.info("Import complete. %r tables were imported", len(table_descriptions))
+
+    access_token = information_territoire_access_token().as_setup()
+
+    processed = registered_jobseeker_stats_by_territory(access_token)
+
+    end = slack.success_notifying_task()
+
+    (
+        python.PythonOperator(task_id="create_schema", python_callable=db.create_schema, op_args=[DB_SCHEMA])
+        >> processed
+        >> end
+    )
