8e8710c6ae2c0a50caafe5a5d8165117f0715f9a
Giulia Carra <giulia.carra@gmail.com>
2025-02-27 11:18:21 +0100

    change style first changes

--- dbt/models/marts/weekly/parcours_par_salarie.sql
+++ dbt/models/marts/weekly/parcours_par_salarie.sql
@@ -1,17 +1,16 @@
--- WIP
-SELECT
+select
 	salarie.hash_nir,
-	COUNT(DISTINCT pass_ag.hash_numéro_pass_iae) AS nombre_pass,
-	MIN(pass_ag.date_début) AS date_début_premier_pass,
-	MAX(pass_ag.date_fin) AS date_fin_dernier_pass,
-	CASE
-		WHEN MAX(pass_ag.date_fin) > CURRENT_DATE THEN 'oui'
-		ELSE 'non'
-	END AS personne_en_parcours,
-	SUM(pass_ag.durée) AS somme_durées_tous_pass,
-	EXTRACT(YEAR FROM MIN(pass_ag.date_début)) AS annee_début_premier_pass
-FROM pass_agréments AS pass_ag
-LEFT JOIN "fluxIAE_Salarie" AS salarie
-ON pass_ag.hash_numéro_pass_iae = salarie.hash_numéro_pass_iae
-WHERE pass_ag.type != 'Agrément PE'
-GROUP BY salarie.hash_nir
+	count(distinct pass_ag."hash_numéro_pass_iae") as nombre_pass,
+	min(pass_ag.date_début) as date_début_premier_pass,
+	max(pass_ag.date_fin) as date_fin_dernier_pass,
+	case
+		when max(pass_ag.date_fin) > CURRENT_DATE then 'oui'
+		else 'non'
+	end as personne_en_parcours,
+	sum(pass_ag.durée) as somme_durées_tous_pass,
+	extract(year from min(pass_ag.date_début)) as annee_début_premier_pass
+from pass_agréments as pass_ag
+left join "fluxIAE_Salarie" as salarie
+on pass_ag.hash_numéro_pass_iae = salarie.hash_numéro_pass_iae
+where pass_ag.type != 'Agrément PE'
+group by salarie.hash_nir
