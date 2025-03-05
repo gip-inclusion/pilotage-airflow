2a576cd73ab31a0a7f6fa814a00e053ac57ba717
laurinehu <laurinehu@gmail.com>
2025-03-05 07:45:15 +0100

    solve contracts duplicate issue and add test

--- dbt/models/staging/stg_contrats.sql
+++ dbt/models/staging/stg_contrats.sql
@@ -19,7 +19,7 @@ from {{ ref('fluxIAE_EtatMensuelIndiv_v2') }} as emi
 left join {{ ref('fluxIAE_ContratMission_v2') }} as ctr
     on emi.emi_ctr_id = ctr.contrat_id_ctr
 left join {{ source("fluxIAE", "fluxIAE_RefFormeContrat") }} as rfc
-    on ctr.contrat_format_contrat_code = rfc.rfc_code_forme_contrat
+    on ctr.contrat_format_contrat_code = rfc.rfc_id
 left join {{ ref('fluxIAE_RefMotifSort_v2') }} as motif_sortie
     on emi.emi_motif_sortie_id = motif_sortie.rms_id
 group by
