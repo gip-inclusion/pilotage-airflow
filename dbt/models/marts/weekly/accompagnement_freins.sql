27900caf077e206ba0aef7fda8b5643373b80c1d
Giulia Carra <giulia.carra@gmail.com>
2025-03-18 14:08:01 +0100

    added ratio to accompagnement_freins table

--- dbt/models/marts/weekly/accompagnement_freins.sql
+++ dbt/models/marts/weekly/accompagnement_freins.sql
@@ -6,6 +6,10 @@ select
     t_struct.nom_departement_structure,
     t_struct.code_dept_structure             as numero_departement_structure,
     t_struct.nom_region_structure,
+    t_acc.acc_dif_nb_sal_acc/t_acc.acc_dif_nb_sal_conc as ratio_salaries_accompagnes,
+    t_acc.acc_dif_nb_sal_int/t_acc.acc_dif_nb_sal_acc as ratio_int_vs_acc,
+    t_acc.acc_dif_nb_sal_ext/t_acc.acc_dif_nb_sal_acc as ratio_ext_vs_acc,
+    t_acc.acc_dif_nb_sal_int_ext/t_acc.acc_dif_nb_sal_acc as ratio_int_ext_vs_acc,
     extract(year from t_afi.af_date_debut_effet_v2) as annee_af
 from {{ ref('stg_accompagnement_freins') }} as t_acc
 left join {{ ref('stg_info_per_annexe_financiere') }} as t_afi on t_acc.acc_afi_id = t_afi.af_id_annexe_financiere
