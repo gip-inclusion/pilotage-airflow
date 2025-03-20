a9ed2be3f5df203b2ce1bce966169e5cad1bd4d0
Giulia Carra <giulia.carra@gmail.com>
2025-03-20 17:00:28 +0100

    add table accompagnement_pro

--- dbt/tests/test_stg_accompagnement_freins.sql
+++ dbt/tests/test_stg_accompagnement_freins.sql
@@ -1,9 +1,9 @@
 select
     acc_id,
     type_frein,
-    sum(acc_dif_nb_sal_acc)  as total_salaries,
-    sum(acc_dif_nb_sal_conc) as total_salaries_conc,
-    count(*)                 as nb
+    sum(acc_nb_sal_acc)  as total_salaries,
+    sum(acc_nb_sal_conc) as total_salaries_conc,
+    count(*)             as nb
 from {{ ref("stg_accompagnement_freins") }}
 group by acc_id, type_frein
-having count(*) > 1 or (sum(acc_dif_nb_sal_acc) > sum(acc_dif_nb_sal_conc))
+having count(*) > 1 or (sum(acc_nb_sal_acc) > sum(acc_nb_sal_conc))
