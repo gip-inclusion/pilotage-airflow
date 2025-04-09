817b470d2633468293f23725472e75810aedbffa
Romain Sébille <romain.sebille@beta.gouv.fr>
2025-04-09 12:45:06 +0200

    requirements: Update all development ones
    Also fix sqlfluff `layout.keyword_newline`:
    https://docs.sqlfluff.com/en/stable/reference/rules.html#rule-layout.keyword_newline

--- dbt/models/marts/weekly/contrats_aci.sql
+++ dbt/models/marts/weekly/contrats_aci.sql
@@ -2,32 +2,41 @@ select
     ctr.contrat_id_structure,
     ctr.contrat_id_pph,
     salarie.hash_nir,
-    count(distinct ctr.type_contrat)                                    as types_contrats_distincts,
-    array_agg(ctr.type_contrat order by ctr.contrat_date_embauche)      as types_contrats,
-    (array_agg(ctr.type_contrat order by ctr.contrat_date_embauche))[1] as type_contrat_embauche,
-    max(structs.code_dept_structure)                                    as departement_structure,
-    max(structs.nom_departement_structure)                              as nom_departement_structure,
-    max(structs.nom_region_structure)                                   as nom_region_structure,
-    max(motif_sortie)                                                   as motif_sortie,
+    count(distinct ctr.type_contrat)          as types_contrats_distincts,
+    array_agg(
+        ctr.type_contrat
+        order by ctr.contrat_date_embauche
+    )                                         as types_contrats,
+    (array_agg(
+        ctr.type_contrat
+        order by ctr.contrat_date_embauche
+    ))[1]                                     as type_contrat_embauche,
+    max(structs.code_dept_structure)          as departement_structure,
+    max(structs.nom_departement_structure)    as nom_departement_structure,
+    max(structs.nom_region_structure)         as nom_region_structure,
+    max(motif_sortie)                         as motif_sortie,
     case
         when max(motif_sortie) is null then 'Non'
         else 'Oui'
-    end                                                                 as salarie_sorti,
-    array_agg(ctr.contrat_id_ctr order by ctr.contrat_date_embauche)    as id_contrats,
-    max(contrat_mesure_disp_code)                                       as type_aci,
-    min(ctr.contrat_date_embauche)                                      as date_embauche_premier_contrat,
-    max(ctr.contrat_date_fin_contrat)                                   as date_fin_dernier_contrat,
-    max(ctr.contrat_date_sortie_definitive)                             as date_sortie_definitive_dernier_contrat,
+    end                                       as salarie_sorti,
+    array_agg(
+        ctr.contrat_id_ctr
+        order by ctr.contrat_date_embauche
+    )                                         as id_contrats,
+    max(contrat_mesure_disp_code)             as type_aci,
+    min(ctr.contrat_date_embauche)            as date_embauche_premier_contrat,
+    max(ctr.contrat_date_fin_contrat)         as date_fin_dernier_contrat,
+    max(ctr.contrat_date_sortie_definitive)   as date_sortie_definitive_dernier_contrat,
     case
         when max(ctr.contrat_date_sortie_definitive) is not null then {{ duration_in_days('min(ctr.contrat_date_embauche)', 'max(ctr.contrat_date_sortie_definitive)') }}
         else {{ duration_in_days('min(ctr.contrat_date_embauche)', 'max(ctr.contrat_date_fin_contrat)') }}
-    end                                                                 as duree_contrat_jours,
+    end                                       as duree_contrat_jours,
     case
         when max(ctr.contrat_date_sortie_definitive) is not null then {{ duration_in_months('min(ctr.contrat_date_embauche)', 'max(ctr.contrat_date_sortie_definitive)') }}
         else {{ duration_in_months('min(ctr.contrat_date_embauche)', 'max(ctr.contrat_date_fin_contrat)') }}
-    end                                                                 as duree_contrat_mois,
-    sum(ctr.contrat_duree_contrat)                                      as duree_contrat_asp_mois,
-    max(struct_emplois.structure_convergence)                           as structure_convergence
+    end                                       as duree_contrat_mois,
+    sum(ctr.contrat_duree_contrat)            as duree_contrat_asp_mois,
+    max(struct_emplois.structure_convergence) as structure_convergence
 from {{ ref('stg_contrats') }} as ctr
 left join {{ ref("fluxIAE_Structure_v2") }} as structs
     on ctr.contrat_id_structure = structs.structure_id_siae
--- dbt/models/staging/stg_parcours_salarie_info_pass.sql
+++ dbt/models/staging/stg_parcours_salarie_info_pass.sql
@@ -1,15 +1,18 @@
 select
     salarie.hash_nir,
-    count(distinct pass_ag."hash_numéro_pass_iae")               as nombre_pass,
-    min(pass_ag."date_début")                                    as "date_début_premier_pass",
-    max(pass_ag.date_fin)                                        as date_fin_dernier_pass,
+    count(distinct pass_ag."hash_numéro_pass_iae")        as nombre_pass,
+    min(pass_ag."date_début")                             as "date_début_premier_pass",
+    max(pass_ag.date_fin)                                 as date_fin_dernier_pass,
     case
         when max(pass_ag.date_fin) > current_date then 'Non'
         else 'Oui'
-    end                                                          as sortie_du_parcours,
-    sum(extract(day from pass_ag."durée")::float) / 30.44        as "somme_durées_tous_pass_mois",
-    array_agg(pass_ag."durée" order by pass_ag."date_début" asc) as "liste_durées_pass",
-    extract(year from min(pass_ag."date_début"))                 as "annee_début_premier_pass"
+    end                                                   as sortie_du_parcours,
+    sum(extract(day from pass_ag."durée")::float) / 30.44 as "somme_durées_tous_pass_mois",
+    array_agg(
+        pass_ag."durée"
+        order by pass_ag."date_début" asc
+    )                                                     as "liste_durées_pass",
+    extract(year from min(pass_ag."date_début"))          as "annee_début_premier_pass"
 from {{ source("emplois","pass_agréments") }} as pass_ag
 left join {{ source("fluxIAE","fluxIAE_Salarie") }} as salarie
     on pass_ag."hash_numéro_pass_iae" = salarie."hash_numéro_pass_iae"
