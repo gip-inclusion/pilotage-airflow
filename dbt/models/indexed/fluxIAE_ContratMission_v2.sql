15f379262b15356f3e77f45217d490aa40380426
Yannick Passarelli <yannick.passarelli@gmail.com>
2025-04-23 19:40:08 +0200

    creating a beneficiaires file

--- dbt/models/indexed/fluxIAE_ContratMission_v2.sql
+++ dbt/models/indexed/fluxIAE_ContratMission_v2.sql
@@ -7,7 +7,13 @@
 
 select
     {{ pilo_star(source('fluxIAE', 'fluxIAE_ContratMission'), except=['contrat_date_embauche', "contrat_date_fin_contrat"]) }},
-    to_date(contrat_date_embauche, 'DD/MM/YYYY')    as contrat_date_embauche,
-    to_date(contrat_date_fin_contrat, 'DD/MM/YYYY') as contrat_date_fin_contrat
+    to_date(ctr.contrat_date_embauche, 'DD/MM/YYYY')    as contrat_date_embauche,
+    to_date(ctr.contrat_date_fin_contrat, 'DD/MM/YYYY') as contrat_date_fin_contrat,
+    rnf.rnf_libelle_niveau_form_empl                    as niveau_de_formation,
+    rdp.rdp_lib_duree_pe                                as duree_inscription_ft
 from
-    {{ source('fluxIAE', 'fluxIAE_ContratMission') }}
+    {{ source('fluxIAE', 'fluxIAE_ContratMission') }} as ctr
+left join {{ source('fluxIAE', 'fluxIAE_RefNiveauFormation') }} as rnf
+    on ctr.contrat_niveau_de_formation_id = rnf.rnf_id
+left join {{ source('fluxIAE', 'fluxIAE_RefDureePoleEmploi') }} as rdp
+    on ctr.contrat_salarie_pe_duree_id = rdp.rdp_id
