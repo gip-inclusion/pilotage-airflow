6c3466cf49963615cc5c2775ebc7fb97001a9936
laurinehu <laurinehu@gmail.com>
2025-05-27 17:56:43 +0200

    Add pass informations : interupted pass + bassin emploi of the structure

--- dbt/models/marts/daily/pass_agrements_valides.sql
+++ dbt/models/marts/daily/pass_agrements_valides.sql
@@ -1,9 +1,16 @@
 select
     {{ pilo_star(source('emplois', 'pass_agréments'), relation_alias="pa") }},
+    struct.bassin_d_emploi as bassin_emploi_structure,
     case
         when
             pa.date_fin >= current_date then 'pass valide'
         else 'pass non valide'
-    end as validite_pass
+    end                    as validite_pass,
+    suspensions.suspension_en_cours,
+    suspensions.motif_suspension
 from
     {{ source('emplois', 'pass_agréments') }} as pa
+left join {{ ref('stg_structures') }} as struct
+    on struct.id = pa.id_structure
+left join {{ ref('suspensions_pass') }} as suspensions
+    on suspensions."id_pass_agrément" = pa.id
