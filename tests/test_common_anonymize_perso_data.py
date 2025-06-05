3bb1d978dad61f8faca0bf5401cb4868f8c20438
Giulia Carra <giulia.carra@gmail.com>
2025-06-05 17:13:11 +0200

    make quality again

--- tests/test_common_anonymize_perso_data.py
+++ tests/test_common_anonymize_perso_data.py
@@ -1,14 +1,19 @@
 from datetime import datetime
+
 import pytest
 
 from dags.common.anonymize_sensible_data import normalize_sensible_data
 
-@pytest.mark.parametrize("first_name, last_name", [
-    ("Jean", "Dupont"),
-    ("Jéan", "Dùpont"),
-    ("Jeaœn!", "Dupont@"),
-    ("Je-An", "Du Po_nt"),
-])
+
+@pytest.mark.parametrize(
+    "first_name, last_name",
+    [
+        ("Jean", "Dupont"),
+        ("Jéan", "Dùpont"),
+        ("Jeaœn!", "Dupont@"),
+        ("Je-An", "Du Po_nt"),
+    ],
+)
 def test_normalization(first_name, last_name):
     birth_date = datetime(1990, 1, 1).isoformat()
     expected = "jean-dupont-19900101t000000"  # Expected output for all cases
