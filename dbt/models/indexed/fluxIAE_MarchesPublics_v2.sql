49679f49fad1680f82d9382d9544aaaa218507d2
Yannick Passarelli <yannick.passarelli@gmail.com>
2025-06-10 15:58:50 +0200

    remove duplicates from marche public and remove it from etp conventionnes

--- dbt/models/indexed/fluxIAE_MarchesPublics_v2.sql
+++ dbt/models/indexed/fluxIAE_MarchesPublics_v2.sql
@@ -5,7 +5,33 @@
     ]
  ) }}
 
-select distinct --sometimes there are duplicates, we want to avoid this
+with ranked_data as (
+    select
+        mpu_af_id,
+        mpu_sct_mt_recet_nettoyage,
+        mpu_sct_mt_recet_serv_pers,
+        mpu_sct_mt_recet_btp,
+        mpu_sct_mt_recet_agri,
+        mpu_sct_mt_recet_recycl,
+        mpu_sct_mt_recet_transp,
+        mpu_sct_mt_recet_autres,
+        row_number() over (
+            partition by mpu_af_id
+            order by to_timestamp(mpu_date_creation, 'DD/MM/YYYY HH24:MI:SS') desc
+        ) as rn
+    from {{ source('fluxIAE', 'fluxIAE_MarchesPublics') }}
+    where {{ sum_nullable_columns([
+        'mpu_sct_mt_recet_nettoyage',
+        'mpu_sct_mt_recet_serv_pers',
+        'mpu_sct_mt_recet_btp',
+        'mpu_sct_mt_recet_agri',
+        'mpu_sct_mt_recet_recycl',
+        'mpu_sct_mt_recet_transp',
+        'mpu_sct_mt_recet_autres'
+    ]) }} != 0
+)
+
+select
     mpu_af_id,
     mpu_sct_mt_recet_nettoyage,
     mpu_sct_mt_recet_serv_pers,
@@ -14,14 +40,5 @@ select distinct --sometimes there are duplicates, we want to avoid this
     mpu_sct_mt_recet_recycl,
     mpu_sct_mt_recet_transp,
     mpu_sct_mt_recet_autres
-from
-    {{ source('fluxIAE', 'fluxIAE_MarchesPublics') }}
-where {{ sum_nullable_columns([
-    'mpu_sct_mt_recet_nettoyage',
-    'mpu_sct_mt_recet_serv_pers',
-    'mpu_sct_mt_recet_btp',
-    'mpu_sct_mt_recet_agri',
-    'mpu_sct_mt_recet_recycl',
-    'mpu_sct_mt_recet_transp',
-    'mpu_sct_mt_recet_autres'
-]) }} != 0
+from ranked_data
+where rn = 1
