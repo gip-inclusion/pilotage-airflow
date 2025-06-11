cff2eb4b1e5cfe3cbef8367b784479ff292c84fa
Yannick Passarelli <yannick.passarelli@gmail.com>
2025-06-11 11:14:36 +0200

    adding regions to monrecap

--- dbt/models/monrecap/marts/Commandes.sql
+++ dbt/models/monrecap/marts/Commandes.sql
@@ -1,1 +1,6 @@
-select {{ pilo_star(source('monrecap','Commandes_v0')) }} from {{ source('monrecap', 'Commandes_v0') }}
+select
+    {{ pilo_star(source('monrecap','Commandes_v0')) }},
+    reg.region
+from {{ source('monrecap', 'Commandes_v0') }} as cmd
+left join {{ ref('dep_reg_ref_emplois') }} as reg
+    on cmd."Nom Departement" = reg.departement
--- dbt/models/monrecap/staging/stg_departement_derniere_commandes.sql
+++ dbt/models/monrecap/staging/stg_departement_derniere_commandes.sql
@@ -1,8 +1,12 @@
 select
-    "Email"                  as email_commande,
-    max("Nom Departement")   as nom_departement,
-    max("Submitted at")      as "Submitted at",
-    max("Nombre de Carnets") as "Nombre de Carnets"
-from {{ source('monrecap','Commandes_v0') }}
+    cmd."Email"                  as email_commande,
+    reg.region,
+    max(cmd."Nom Departement")   as nom_departement,
+    max(cmd."Submitted at")      as "Submitted at",
+    max(cmd."Nombre de Carnets") as "Nombre de Carnets"
+from {{ source('monrecap','Commandes_v0') }} as cmd
+left join {{ ref('dep_reg_ref_emplois') }} as reg
+    on cmd."Nom Departement" = reg.departement
 group by
-    "Email"
+    cmd."Email",
+    reg.region
