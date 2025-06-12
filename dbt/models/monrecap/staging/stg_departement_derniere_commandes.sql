4a1abe333b2c78c49b98deb04f38742d03c6adc3
Yannick Passarelli <yannick.passarelli@gmail.com>
2025-06-12 09:05:44 +0200

    fix duplicate in regions

--- dbt/models/monrecap/staging/stg_departement_derniere_commandes.sql
+++ dbt/models/monrecap/staging/stg_departement_derniere_commandes.sql
@@ -1,12 +1,17 @@
+with departement_table as (
+    select
+        cmd."Email"                  as email_commande,
+        max(cmd."Nom Departement")   as nom_departement,
+        max(cmd."Submitted at")      as "Submitted at",
+        max(cmd."Nombre de Carnets") as "Nombre de Carnets"
+    from {{ source('monrecap','Commandes_v0') }} as cmd
+    group by
+        cmd."Email"
+)
+
 select
-    cmd."Email"                  as email_commande,
-    reg.region,
-    max(cmd."Nom Departement")   as nom_departement,
-    max(cmd."Submitted at")      as "Submitted at",
-    max(cmd."Nombre de Carnets") as "Nombre de Carnets"
-from {{ source('monrecap','Commandes_v0') }} as cmd
-left join {{ ref('dep_reg_ref_emplois') }} as reg
-    on cmd."Nom Departement" = reg.departement
-group by
-    cmd."Email",
+    dpt.*,
     reg.region
+from departement_table as dpt
+left join {{ ref('dep_reg_ref_emplois') }} as reg
+    on dpt.nom_departement = reg.departement
