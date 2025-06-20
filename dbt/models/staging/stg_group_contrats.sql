d2ee296228d4dd96b4f386ba3e1a68585afb4d1f
Giulia Carra <giulia.carra@gmail.com>
2025-06-20 18:05:49 +0200

    change stg_group_contrats agregation

--- dbt/models/staging/stg_group_contrats.sql
+++ dbt/models/staging/stg_group_contrats.sql
@@ -4,7 +4,7 @@ select
         when ctr.contrat_type_contrat = 0 then 1
         else 0
     end) over (
-        partition by ctr.contrat_id_pph, ctr.contrat_mesure_disp_code
+        partition by ctr.contrat_id_pph, ctr.contrat_mesure_disp_code, ctr.contrat_id_structure
         order by ctr.contrat_date_embauche
     ) as groupe_contrat
 from {{ ref('fluxIAE_ContratMission_v2') }} as ctr
