90d5ac39ea684c6bdd91f55a9a4e825d8c07cdcf
Romain Sébille <romain.sebille@beta.gouv.fr>
2025-06-25 12:12:44 +0200

    airflow: Fix S3 connections when using Clever Cloud Cellar

--- custom-prod-entrypoint.sh
+++ custom-prod-entrypoint.sh
@@ -12,7 +12,7 @@ export AIRFLOW__DATABASE__SQL_ALCHEMY_CONN="${POSTGRESQL_ADDON_URI//\"/}"
 airflow db migrate
 
 if [[ "x$CELLAR_ADDON_HOST" != "x" ]]; then
-    export AIRFLOW_CONN_LOG_CONNECTION="aws://@/?endpoint_url=https%3A%2F%2F${CELLAR_ADDON_HOST//\"/}&region_name=eu-west-1&aws_access_key_id=${CELLAR_ADDON_KEY_ID//\"/}&aws_secret_access_key=${CELLAR_ADDON_KEY_SECRET//\"/}"
+    export AIRFLOW_CONN_LOG_CONNECTION="aws:///?__extra__=%7B%22endpoint_url%22%3A+%22https%3A%2F%2F${CELLAR_ADDON_HOST//\"/}%22%2C+%22aws_access_key_id%22%3A+%22${CELLAR_ADDON_KEY_ID//\"/}%22%2C+%22aws_secret_access_key%22%3A+%22${CELLAR_ADDON_KEY_SECRET//\"/}%22%2C+%22config_kwargs%22%3A+%7B%22request_checksum_calculation%22%3A+%22when_required%22%2C+%22response_checksum_validation%22%3A+%22when_required%22%7D%7D"
     export AIRFLOW__LOGGING__REMOTE_LOGGING=True
     export AIRFLOW__LOGGING__REMOTE_BASE_LOG_FOLDER="s3://${LOG_BUCKET_NAME//\"/}/logs"
     export AIRFLOW__LOGGING__REMOTE_LOG_CONN_ID="log_connection"
