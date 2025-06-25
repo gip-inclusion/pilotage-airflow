27fb3e74532aa074e8fe2a056e96885706b5b74f
Romain Sébille <romain.sebille@beta.gouv.fr>
2025-06-25 16:37:02 +0200

    dags.ft_information_territoire: Make some improvements to the DAG

--- dags/ft_information_territoire.py
+++ dags/ft_information_territoire.py
@@ -1,6 +1,7 @@
 import logging
 
 from airflow import DAG
+from airflow.decorators import task
 from airflow.operators import python
 
 from dags.common import default_dag_args, slack
@@ -14,6 +15,7 @@ logger = logging.getLogger(__name__)
 # we cannot reliably schedule this DAG, so we run it regularly.
 with DAG(**default_dag_args(), dag_id="ft_information_territoire", schedule_interval="@weekly") as dag:
 
+    @task(task_id="registered_jobseeker_stats_by_territory")
     def registered_jobseeker_stats_by_territory(**kwargs):
         # Import data from the API for each territory targeted.
         access_token = ft_api_helpers.request_access_token(format_for_header=True)
@@ -23,12 +25,8 @@ with DAG(**default_dag_args(), dag_id="ft_information_territoire", schedule_inte
 
         logger.info("Import complete.")
 
-    end = slack.success_notifying_task()
-
     (
-        python.PythonOperator(task_id="create_schema_and_tables", python_callable=models.create_tables)
-        >> python.PythonOperator(
-            task_id="registered_jobseeker_stats_by_territory", python_callable=registered_jobseeker_stats_by_territory
-        )
-        >> end
+        python.PythonOperator(task_id="create_schema_and_tables", python_callable=models.create_tables).as_setup()
+        >> registered_jobseeker_stats_by_territory()
+        >> slack.success_notifying_task()
     )
