0580a861b7687e632f5ee88e970d0386c5efdbd6
Yannick Passarelli <yannick.passarelli@gmail.com>
2025-07-01 14:03:30 +0200

    quick and dirty fix, the whole dag will be changed next week

--- dbt/models/monrecap/marts/Commandes.sql
+++ dbt/models/monrecap/marts/Commandes.sql
@@ -1,5 +1,6 @@
 select
-    {{ pilo_star(source('monrecap','Commandes_v0')) }},
+    {{ pilo_star(source('monrecap','Commandes_v0'), except =["declaratif quels freins"]) }},
+    cmd."declaratif quels freins"::text as "declaratif quels freins",
     reg.region
 from {{ source('monrecap', 'Commandes_v0') }} as cmd
 left join {{ ref('dep_reg_ref_emplois') }} as reg
