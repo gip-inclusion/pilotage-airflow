ec0a3a1fe3ebe59c2396e6117fff1a2ebd1dba53
Giulia Carra <giulia.carra@gmail.com>
2025-07-03 18:25:43 +0200

    small quality modifs

--- dbt/models/marts/weekly/les_emplois_contrats.sql
+++ dbt/models/marts/weekly/les_emplois_contrats.sql
@@ -8,24 +8,23 @@ select
     case
         when ctr.contrat_type_contrat = 0 then 'contrat_initiale'
         when ctr.contrat_type_contrat > 0 then 'renouvellement'
-        else null
-    end as type_contrat,
+    end                                         as type_contrat,
     ctr.contrat_date_sortie_definitive,
     ctr.contrat_date_embauche,
     ctr.contrat_date_fin_contrat,
     pass_ag."hash_numéro_pass_iae",
-    pass_ag.id as pass_iae_id,
-    pass_ag."date_début" as pass_iae_date_debut,
-    pass_ag.date_fin as pass_iae_date_fin
+    pass_ag.id                                  as pass_iae_id,
+    pass_ag."date_début"                        as pass_iae_date_debut,
+    pass_ag.date_fin                            as pass_iae_date_fin
 from {{ ref("fluxIAE_ContratMission_v2") }} as ctr
 left join {{ ref('ref_mesure_dispositif_asp') }} as type_structure
-on ctr.contrat_mesure_disp_code = type_structure.af_mesure_dispositif_code
-left join {{ ref("fluxIAE_Structure_v2")}} as structs
-on ctr.contrat_id_structure = structs.structure_id_siae
+    on ctr.contrat_mesure_disp_code = type_structure.af_mesure_dispositif_code
+left join {{ ref("fluxIAE_Structure_v2") }} as structs
+    on ctr.contrat_id_structure = structs.structure_id_siae
 left join {{ source('fluxIAE',"fluxIAE_Salarie") }} salarie
-on ctr.contrat_id_pph = salarie.salarie_id
+    on ctr.contrat_id_pph = salarie.salarie_id
 left join {{ source('emplois', 'pass_agréments') }} as pass_ag
-on salarie."hash_numéro_pass_iae" = pass_ag."hash_numéro_pass_iae"
-where pass_ag.type != 'Agrément PE'
-    and pass_ag."date_début" <= ctr.contrat_date_embauche
-    and pass_ag.date_fin >= to_date(ctr.contrat_date_sortie_definitive, 'DD/MM/YYYY')
+    on salarie."hash_numéro_pass_iae" = pass_ag."hash_numéro_pass_iae"
+where pass_ag.type != 'Agrément PE' and
+    pass_ag."date_début" <= ctr.contrat_date_embauche and
+    pass_ag.date_fin >= to_date(ctr.contrat_date_sortie_definitive, 'DD/MM/YYYY')
