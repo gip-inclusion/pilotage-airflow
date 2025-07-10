d43af7a71783c564d9ac6b4bc616b2d4d47bd0f2
Giulia Carra <giulia.carra@gmail.com>
2025-07-10 10:47:37 +0200

    modified pass_agrements_valides to avoid duplicates

--- dbt/models/marts/daily/pass_agrements_valides.sql
+++ dbt/models/marts/daily/pass_agrements_valides.sql
@@ -1,16 +1,21 @@
 select
     {{ pilo_star(source('emplois', 'pass_agréments'), relation_alias="pa") }},
     struct.bassin_d_emploi as bassin_emploi_structure,
+    suspensions.motif_suspension,
     case
         when
             pa.date_fin >= current_date then 'pass valide'
         else 'pass non valide'
     end                    as validite_pass,
-    suspensions.suspension_en_cours,
-    suspensions.motif_suspension
+    case
+        when
+            suspensions."id_pass_agrément" is null then 'Non'
+        else 'Oui'
+    end                    as suspension_en_cours
 from
     {{ source('emplois', 'pass_agréments') }} as pa
 left join {{ ref('stg_structures') }} as struct
-    on struct.id = pa.id_structure
-left join {{ ref('suspensions_pass') }} as suspensions
-    on suspensions."id_pass_agrément" = pa.id
+    on pa.id_structure = struct.id
+left join {{ ref('eph_suspensions_pass_en_cours') }} as suspensions
+    on pa.id = suspensions."id_pass_agrément"
+where pa.type != 'Agrément PE'
