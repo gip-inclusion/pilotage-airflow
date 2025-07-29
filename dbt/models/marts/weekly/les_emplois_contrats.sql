966df1bc5f333d71c5709c8363709c339bc6c2c2
Romain Sébille <romain.sebille@beta.gouv.fr>
2025-07-29 09:18:06 +0200

    models: Simplify `les_emplois_contrats`
    - Reduce the number of informations returned as they can be
      found with the user ID or the SIAE ID and its kind.
    - Add `contrat_parent_id` to ease grouping

--- dbt/models/marts/weekly/les_emplois_contrats.sql
+++ dbt/models/marts/weekly/les_emplois_contrats.sql
@@ -1,26 +1,18 @@
 select
-    candidats.id             as candidat_id,
-    salarie.hash_nir,
-    ctr.contrat_id_structure as id_structure,
-    structs.structure_denomination,
-    structs.structure_siret_actualise,
-    type_structure.type_structure_emplois,
-    salarie.salarie_id,
+    candidats.id as emplois_candidat_id,
+    ctr.contrat_id_structure,
+    ctr.contrat_mesure_disp_code,
     ctr.contrat_id_ctr,
-    ctr.contrat_date_sortie_definitive,
+    ctr.contrat_parent_id,
     ctr.contrat_date_embauche,
     ctr.contrat_date_fin_contrat,
+    ctr.contrat_date_sortie_definitive,
     case
-        when ctr.contrat_type_contrat = 0 then 'contrat_initiale'
-        when ctr.contrat_type_contrat > 0 then 'renouvellement'
-    end                      as type_contrat
-from {{ ref("fluxIAE_ContratMission_v2") }} as ctr
-left join {{ ref('ref_mesure_dispositif_asp') }} as type_structure
-    on ctr.contrat_mesure_disp_code = type_structure.af_mesure_dispositif_code
-left join {{ ref("fluxIAE_Structure_v2") }} as structs
-    on ctr.contrat_id_structure = structs.structure_id_siae
-left join {{ ref("stg_uniques_salarie_id") }} as salarie
+        when ctr.num_reconduction = 0 then 'initial'
+        when ctr.num_reconduction > 0 then 'renouvellement'
+    end          as type_contrat
+from {{ ref("stg_contrats") }} as ctr
+inner join {{ ref("stg_uniques_salarie_id") }} as salarie
     on ctr.contrat_id_pph = salarie.salarie_id
-left join {{ source('emplois', 'candidats_v0') }} as candidats
+inner join {{ source('emplois', 'candidats_v0') }} as candidats
     on salarie.hash_nir = candidats.hash_nir
-where candidats.id is not null
