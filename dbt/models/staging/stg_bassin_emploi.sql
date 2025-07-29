51d22311c9eaf11a5a125690d92039415a0e9bc0
Romain Sébille <romain.sebille@beta.gouv.fr>
2025-07-29 14:39:41 +0200

    dbt.seeds: Import `sa_zones_infradepartementales`

--- dbt/models/staging/stg_bassin_emploi.sql
+++ dbt/models/staging/stg_bassin_emploi.sql
@@ -10,7 +10,7 @@ select
     be.nom_zone_emploi_2020 as bassin_d_emploi,
     /* on récupère que l'id des structures de la table structure */
     s.id                    as id_structure
-from {{ source('oneshot', 'sa_zones_infradepartementales') }} as be
+from {{ ref('sa_zones_infradepartementales') }} as be
 left join {{ ref('structures') }} as s
     /* il faut rajouter le département car la France n'est pas originale en terme de noms de ville */
     on s.ville = be.libelle_commune and s."nom_département" = be.nom_departement
