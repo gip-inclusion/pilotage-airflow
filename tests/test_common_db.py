9548e4f605a93089078842f1d2a76ee5df1934d3
Romain Sébille <romain.sebille@beta.gouv.fr>
2025-08-28 16:22:52 +0200

    tests: Use `monkeypatch.setenv()` over `mock.patch.dict("os.environ")`

--- tests/test_common_db.py
+++ tests/test_common_db.py
@@ -1,22 +1,15 @@
-from unittest import mock
-
 from dags.common import db
 
 
-@mock.patch.dict(
-    "os.environ",
-    {
-        "AIRFLOW_VAR_PGHOST": "host",
-        "AIRFLOW_VAR_PGPORT": "port",
-        "AIRFLOW_VAR_PGDATABASE": "db",
-        "AIRFLOW_VAR_PGUSER": "user",
-        "AIRFLOW_VAR_PGPASSWORD": "password",
-    },
-)
-def test_connection_envvars():
-    envvars = db.connection_envvars()
-    assert envvars["PGDATABASE"] == "db"
-    assert envvars["PGHOST"] == "host"
-    assert envvars["PGPASSWORD"] == "password"
-    assert envvars["PGPORT"] == "port"
-    assert envvars["PGUSER"] == "user"
+def test_connection_envvars(monkeypatch):
+    expected = {
+        "PGHOST": "host",
+        "PGPORT": "port",
+        "PGDATABASE": "db",
+        "PGUSER": "user",
+        "PGPASSWORD": "password",
+    }
+    for name, value in expected.items():
+        monkeypatch.setenv(f"AIRFLOW_VAR_{name}", value)
+
+    assert db.connection_envvars() == expected
