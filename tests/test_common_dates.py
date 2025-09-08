2a990046618c3883f84129d4c0d88bb864cbe75e
Romain Sébille <romain.sebille@beta.gouv.fr>
2025-09-08 12:45:09 +0200

    dags.data_inclusion: Harden date parsing

--- tests/test_common_dates.py
+++ tests/test_common_dates.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 
 import pandas  # init pandas to avoid a crash with freezegun  # noqa: F401
 from freezegun import freeze_time
@@ -8,6 +8,6 @@ from dags.common import dates
 
 @freeze_time("2022-06-08")
 def test_date_functions():
-    assert dates.start_of_week() == datetime(2022, 6, 6)
-    assert dates.start_of_previous_week() == datetime(2022, 5, 30)
-    assert dates.end_of_previous_week() == datetime(2022, 6, 5)
+    assert dates.start_of_week() == datetime.date(2022, 6, 6)
+    assert dates.start_of_previous_week() == datetime.date(2022, 5, 30)
+    assert dates.end_of_previous_week() == datetime.date(2022, 6, 5)
