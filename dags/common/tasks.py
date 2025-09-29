fd478cc37abb845c60d590ae94c6b1def4dac533
Yannick Passarelli <yannick.passarelli@gmail.com>
2025-09-29 12:06:57 +0200

    add an option to drop the raw barometre table

--- dags/common/tasks.py
+++ dags/common/tasks.py
@@ -1,4 +1,5 @@
 from airflow.decorators import task
+from airflow.utils.trigger_rule import TriggerRule
 
 from dags.common import db
 
@@ -8,6 +9,6 @@ def create_schema(schema_name):
     db.create_schema(schema_name)
 
 
-@task
+@task(trigger_rule=TriggerRule.NONE_FAILED)
 def create_models(models_base):
     models_base.metadata.create_all(db.connection_engine())
