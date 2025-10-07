2f7afb1482f20507c9019b7141238a2931f7f000
Yannick Passarelli <yannick.passarelli@gmail.com>
2025-10-07 16:13:34 +0200

    fix decimals introduced to csv

--- dags/reseau_iae_adherents.py
+++ dags/reseau_iae_adherents.py
@@ -8,7 +8,7 @@ from dags.common import db, default_dag_args, slack
 RESEAU_IAE_TABLE_CREATE_SQL = """
 DROP TABLE IF EXISTS {table_name} CASCADE;
 CREATE TABLE {table_name}(
-    "SIRET" VARCHAR(16),
+    "SIRET" VARCHAR(32),
     "Réseau IAE" VARCHAR(64)
 );
 """
@@ -25,6 +25,10 @@ with DAG(
 
         sheet_url = Variable.get("RESEAU_IAE_ADHERENTS_PUB_SHEET_URL")
         print(f"reading reseaux IAE SIRETs at {sheet_url=}")
-        db.pg_store("reseau_iae_adherents", pd.read_csv(sheet_url), RESEAU_IAE_TABLE_CREATE_SQL)
+
+        df_rsx = pd.read_csv(sheet_url)
+        # the coerce is here because sometimes we get bad SIRETS that are not only number
+        df_rsx["SIRET"] = pd.to_numeric(df_rsx["SIRET"], errors="coerce").astype("Int64")
+        db.pg_store("reseau_iae_adherents", df_rsx, RESEAU_IAE_TABLE_CREATE_SQL)
 
     store_gsheets() >> slack.success_notifying_task()
