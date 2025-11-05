4343584a84fa8583bf07d1f45ed58124e0f4121d
Yannick Passarelli <yannick.passarelli@gmail.com>
2025-11-05 11:42:59 +0100

    fix typo

--- dbt/models/marts/daily/gps_logs_users.sql
+++ dbt/models/marts/daily/gps_logs_users.sql
@@ -7,23 +7,25 @@ select
     util.id_structure,
     util.id_organisation,
     util.id_institution,
-    s.nom                    as nom_structure,
-    s.type                   as type_structure,
+    s.nom                                                                       as nom_structure,
+    s.type                                                                      as type_structure,
     s.siret,
-    s."nom_département"      as departement_structure,
-    s."région"               as region_structure,
-    org.nom                  as nom_org,
-    org.type                 as type_org,
+    s."nom_département"                                                         as departement_structure,
+    s."région"                                                                  as region_structure,
+    org.nom                                                                     as nom_org,
+    org.type                                                                    as type_org,
     org.type_complet,
     org.dept_org,
     org."région_org",
-    instit.nom               as nom_instit,
-    instit.type              as type_instit,
-    instit."nom_département" as departement_instit,
-    instit."région"          as region_instit
+    instit.nom                                                                  as nom_instit,
+    instit.type                                                                 as type_instit,
+    instit."nom_département"                                                    as departement_instit,
+    instit."région"                                                             as region_instit,
+    -- we add this column to allow a simple filter by dpt with metabase. We accept the fact that the dpt can be imperfect
+    coalesce(s."nom_département", org.dept_org, instit."nom_département", null) as departement_global
 from {{ source('gps', 'gps_log_data') }} as gps
 left join {{ ref('utilisateurs') }} as util
-    on gps.user_id = util.id_institution
+    on gps.user_id = util.id
 left join {{ ref('structures') }} as s
     on util.id_structure = s.id
 left join {{ ref('organisations') }} as org
