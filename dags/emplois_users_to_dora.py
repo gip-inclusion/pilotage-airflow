5fbb49bf3083c18a6491a3ff70a30c5e55f37851
Giulia Carra <giulia.carra@gmail.com>
2025-12-04 11:42:14 +0100

    create_dag_to_push_emplois_users_to_dora

---
+++ dags/emplois_users_to_dora.py
@@ -0,0 +1,53 @@
+import logging
+
+from airflow import DAG
+from airflow.decorators import task
+from airflow.models import Variable
+
+from dags.common import db, dbt, default_dag_args, slack
+
+
+logger = logging.getLogger(__name__)
+
+dag_args = default_dag_args() | {"default_args": dbt.get_default_args()}
+
+
+with DAG("emplois_users_to_dora", schedule="@weekly", **dag_args) as dag:
+
+    @task
+    def export_users(**kwargs):
+        les_emplois_users = db.create_df_from_db('SELECT * FROM "utilisateurs";')
+        if les_emplois_users is None or les_emplois_users.empty:
+            logger.info("No Les-Emplois users found to export.")
+            return
+        else:
+            logger.info("Retrieved %d lines for Les-Emplois users.", len(les_emplois_users))
+
+        cleaned_data = les_emplois_users.dropna(subset=["email"])
+        data_to_store = cleaned_data.drop(["nom", "prenom"], axis=1).rename(
+            columns={
+                "dernière_connexion": "derniere_connexion",
+                "date_mise_à_jour_metabase": "date_mise_a_jour_metabase",
+            }
+        )
+
+        url = "postgresql://{user}:{password}@{host}:{port}/{database}".format(
+            user=Variable.get("DORA_PGUSER"),
+            password=Variable.get("DORA_PGPASSWORD"),
+            host=Variable.get("DORA_PGHOST"),
+            port=Variable.get("DORA_PGPORT"),
+            database=Variable.get("DORA_PGDATABASE"),
+        )
+
+        engine = db.create_engine(url)
+
+        data_to_store.to_sql(
+            name="les_emplois_utilisateurs",
+            con=engine,
+            schema="les_emplois",
+            if_exists="replace",
+            index=False,
+        )
+        logger.info("Exported %d Les-Emplois users to Dora.", len(data_to_store))
+
+    export_users() >> slack.success_notifying_task()
