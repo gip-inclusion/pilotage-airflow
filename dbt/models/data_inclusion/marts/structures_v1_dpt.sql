4bb526ee844c73f45e6bc05f58c219624c6c8444
Yannick Passarelli <yannick.passarelli@gmail.com>
2026-01-12 16:33:56 +0100

    create raw di tables with department

--- dbt/models/data_inclusion/marts/structures_v1_dpt.sql
+++ dbt/models/data_inclusion/marts/structures_v1_dpt.sql
@@ -1,19 +1,10 @@
 select
     {{ pilo_star(source('data_inclusion','structures_v1'), relation_alias = 'structures') }},
-    reseau_porteur,
-    services.service_thematique,
-    services.service_public,
-    services.service_mode_accueil,
-    geo.nom_region,
-    geo.code_region,
     geo.nom_departement,
     case
         when geo.code_dept like '%97%' then lpad(geo.code_dept, 3, '0')
         else lpad(geo.code_dept, 2, '0')
     end as code_dept
 from {{ source('data_inclusion', 'structures_v1') }} as structures
-left join lateral unnest(reseaux_porteurs) as reseau_porteur on true
-left join {{ ref('services_thematiques_public_accueil_v1') }} as services
-    on structures.id = services.structure_id
 left join {{ ref('stg_insee_appartenance_geo_communes') }} as geo
     on ltrim(structures.code_insee, '0') = geo.code_insee
