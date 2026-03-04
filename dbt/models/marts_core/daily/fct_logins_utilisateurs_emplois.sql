757435fadce12eddf3c59224ea7f13c78b51fbe4
Giulia Carra <giulia.carra@gmail.com>
2026-03-04 11:46:17 +0100

    stock last_login emplois users

---
+++ dbt/models/marts_core/daily/fct_logins_utilisateurs_emplois.sql
@@ -0,0 +1,37 @@
+{{ config(
+    materialized='incremental',
+    unique_key=['user_id', 'last_login']
+) }}
+
+with source as (
+
+    select distinct
+        id                   as user_id,
+        "dernière_connexion" as last_login
+    from {{ source('emplois', 'utilisateurs_v0') }}
+    where "dernière_connexion" is not null
+
+),
+
+new_rows as (
+
+    select *
+    from source
+
+    {% if is_incremental() %}
+        where not exists (
+            select 1
+            from {{ this }} as t
+            where
+                t.user_id = source.user_id
+                and t.last_login = source.last_login
+        )
+    {% endif %}
+
+)
+
+select
+    user_id,
+    last_login,
+    current_timestamp as loaded_at
+from new_rows
