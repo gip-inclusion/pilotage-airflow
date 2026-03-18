5e1d0fa7d0b770e88909da7d06ecaa0ac98d2e2b
Yannick <yannick.passarelli@gmail.com>
2026-03-18 11:00:46 +0100

    fix broken test

--- dbt/tests/test_candidats_recherche_active.sql
+++ dbt/tests/test_candidats_recherche_active.sql
@@ -2,12 +2,12 @@ with nb_candidats_recherche_active as (
     select
         (
             select count(distinct id)
-            from stg_candidats_candidatures
+            from {{ ref('stg_candidats_candidatures') }}
             where date_candidature >= current_date - interval '6 months'
         ) as cdd_stg,
         (
             select count(*)
-            from candidats_recherche_active
+            from {{ ref('candidats_recherche_active') }}
             where date_derniere_candidature >= current_date - interval '6 months'
         ) as cdd_candidats_recherche_active
 )
