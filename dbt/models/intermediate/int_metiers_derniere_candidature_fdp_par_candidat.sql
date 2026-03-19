fd689790d079660403412ec2d359210a730ac3fa
Giulia Carra <giulia.carra.pro@gmail.com>
2026-03-19 10:43:20 +0100

    fix: bug table int_metiers_derniere_candidature_fdp_par_candidat

--- dbt/models/intermediate/int_metiers_derniere_candidature_fdp_par_candidat.sql
+++ dbt/models/intermediate/int_metiers_derniere_candidature_fdp_par_candidat.sql
@@ -1,7 +1,6 @@
 with candidatures as (
-    select
+    select distinct
         date_candidature,
-        id_candidat,
         hash_nir,
         code_rome_fpd as code_rome_fdp,
         nom_rome_fdp
@@ -20,4 +19,4 @@ select candidatures.*
 from candidatures
 inner join derniere_candidature_par_candidat
     on candidatures.hash_nir = derniere_candidature_par_candidat.hash_nir and candidatures.date_candidature = derniere_candidature_par_candidat.date_candidature
-where hash_nir is not null
+where candidatures.hash_nir is not null
