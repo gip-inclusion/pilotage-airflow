6c4e94929b517ee60fd80285c5ab2e750808fe38
Yannick <yannick.passarelli@gmail.com>
2026-04-29 16:31:47 +0200

    feat:refacto of the di's models + deduplicated structures removed

--- dbt/models/marts/data_inclusion/services_thematiques_public_accueil_v1.sql
+++
@@ -1,9 +0,0 @@
-select
-    {{ pilo_star(ref('services_v1')) }},
-    service_thematique,
-    service_public,
-    service_mode_accueil
-from {{ ref('services_v1') }}
-left join lateral unnest(thematiques) as service_thematique on true
-left join lateral unnest(publics) as service_public on true
-left join lateral unnest(modes_accueil) as service_mode_accueil on true
