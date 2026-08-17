a3ad6081cc30852b1eda73f77559a080c03d894e
Yannick <yannick.passarelli@gmail.com>
2026-08-17 16:40:54 +0200

    upgrade to airflow 3

--- custom-prod-entrypoint.sh
+++ custom-prod-entrypoint.sh
@@ -9,7 +9,11 @@ set +o allexport
 
 export AIRFLOW__DATABASE__SQL_ALCHEMY_CONN="${POSTGRESQL_ADDON_URI//\"/}"
 
+export AIRFLOW__API_AUTH__JWT_SECRET="${AIRFLOW__API_AUTH__JWT_SECRET//\"/}"
+
+
 airflow db migrate
+airflow fab-db migrate
 
 if [[ "$CELLAR_ADDON_HOST" != "" ]]; then
     export AIRFLOW_CONN_LOG_CONNECTION="aws:///?__extra__=%7B%22endpoint_url%22%3A+%22https%3A%2F%2F${CELLAR_ADDON_HOST//\"/}%22%2C+%22aws_access_key_id%22%3A+%22${CELLAR_ADDON_KEY_ID//\"/}%22%2C+%22aws_secret_access_key%22%3A+%22${CELLAR_ADDON_KEY_SECRET//\"/}%22%2C+%22config_kwargs%22%3A+%7B%22request_checksum_calculation%22%3A+%22when_required%22%2C+%22response_checksum_validation%22%3A+%22when_required%22%7D%7D"
